085b690eb969320d798a49380b77b1bed672b840
Ben Firshman <ben@firshman.co.uk>
2021-03-20 17:35:47 -0700

    Add input to infer

--- pkg/cli/infer.go
+++ pkg/cli/infer.go
@@ -1,23 +1,29 @@
 package cli
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"net"
+	"mime"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"os/exec"
+	"path"
 	"strings"
 	"time"
 
+	"github.com/schollz/progressbar/v3"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
 	"github.com/replicate/cog/pkg/client"
 )
 
-var outPath string
+var (
+	inputs  []string
+	outPath string
+)
 
 func newInferCommand() *cobra.Command {
 	cmd := &cobra.Command{
@@ -26,7 +32,8 @@ func newInferCommand() *cobra.Command {
 		RunE:  cmdInfer,
 		Args:  cobra.MinimumNArgs(1),
 	}
-	cmd.Flags().StringVarP(&outPath, "output", "o", "", "output path")
+	cmd.Flags().StringArrayVarP(&inputs, "input", "i", []string{}, "Inputs, in the form name=value. if value is prefixed with @, then it is read from a file on disk. E.g. -i path=@image.jpg")
+	cmd.Flags().StringVarP(&outPath, "output", "o", "", "Output path")
 	return cmd
 }
 
@@ -35,6 +42,7 @@ func cmdInfer(cmd *cobra.Command, args []string) error {
 
 	serverUrl := "http://localhost:8080"
 	client := client.NewClient(serverUrl)
+	fmt.Println("--> Loading package", packageId)
 	pkg, err := client.GetPackage(packageId)
 	if err != nil {
 		return err
@@ -42,7 +50,7 @@ func cmdInfer(cmd *cobra.Command, args []string) error {
 
 	artifact := pkg.Artifacts[0]
 
-	fmt.Println("--> Running Docker image", artifact.URI)
+	fmt.Println("--> Pulling and running Docker image", artifact.URI)
 	out, err := exec.Command("docker", "run", "-p", "5000:5000", "-d", artifact.URI).CombinedOutput()
 	if err != nil {
 		fmt.Println(string(out))
@@ -51,12 +59,36 @@ func cmdInfer(cmd *cobra.Command, args []string) error {
 	containerID := strings.TrimSpace(string(out))
 	defer exec.Command("docker", "kill", containerID).CombinedOutput()
 
-	waitForPort("localhost:5000")
-	time.Sleep(3 * time.Second)
+	fmt.Println("--> Waiting for model to load")
+	// TODO timeout
+	waitForHTTP("http://localhost:5000")
 
 	fmt.Println("--> Running inference")
 
-	resp, err := http.Post("http://localhost:5000/infer", "plain/text", bytes.NewBuffer([]byte{}))
+	bodyReader, bodyWriter := io.Pipe()
+	httpClient := &http.Client{}
+	req, err := http.NewRequest(http.MethodPost, "http://localhost:5000/infer", bodyReader)
+	if err != nil {
+		return err
+	}
+	bar := progressbar.DefaultBytes(
+		-1,
+		"uploading",
+	)
+
+	go func() {
+		// TODO: check input files exist before loading model
+		err := writeInputs(req, inputs, io.MultiWriter(bodyWriter, bar))
+		if err != nil {
+			log.Fatal(err)
+		}
+		// MultiWriter is a Writer not a WriteCloser, but zipWriter is a WriteCloser, so we need to Close it ourselves
+		if err = bodyWriter.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	resp, err := httpClient.Do(req)
 	if err != nil {
 		return err
 	}
@@ -67,6 +99,17 @@ func cmdInfer(cmd *cobra.Command, args []string) error {
 	}
 
 	// TODO check content type so we don't barf binary data to stdout
+
+	contentType := resp.Header.Get("Content-Type")
+	mimeType := strings.Split(contentType, ";")[0]
+
+	if mimeType != "plain/text" && outPath == "" {
+		outPath = "output"
+		extension, _ := mime.ExtensionsByType(mimeType)
+		if len(extension) > 0 {
+			outPath += extension[0]
+		}
+	}
 
 	outFile := os.Stdout
 	if outPath != "" {
@@ -87,14 +130,63 @@ func cmdInfer(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-func waitForPort(host string) {
-	timeout := time.Duration(1) * time.Second
+func waitForHTTP(url string) {
+	client := http.Client{
+		Timeout: 1 * time.Second,
+	}
 	for {
-		conn, _ := net.DialTimeout("tcp", host, timeout)
-		if conn != nil {
-			conn.Close()
-			break
+		resp, err := client.Get(url + "/ping")
+		if err == nil && resp.StatusCode == http.StatusOK {
+			return
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
+func writeInputs(req *http.Request, inputs []string, body io.Writer) error {
+	mwriter := multipart.NewWriter(body)
+	req.Header.Add("Content-Type", mwriter.FormDataContentType())
+	defer mwriter.Close()
+
+	for _, input := range inputs {
+		var name, value string
+
+		// Default input name is "input"
+		if !strings.Contains(input, "=") {
+			name = "input"
+			value = input
+		} else {
+			split := strings.SplitN(input, "=", 2)
+			name = split[0]
+			value = split[1]
+		}
+
+		// Treat inputs prefixed with @ as a path to a file
+		if strings.HasPrefix(value, "@") {
+			filepath := value[1:]
+			w, err := mwriter.CreateFormFile(name, path.Base(filepath))
+			if err != nil {
+				return err
+			}
+
+			file, err := os.Open(filepath)
+			if err != nil {
+				return err
+			}
+			if _, err := io.Copy(w, file); err != nil {
+				return err
+			}
+		} else {
+			w, err := mwriter.CreateFormField(name)
+			if err != nil {
+				return err
+			}
+			if _, err = w.Write([]byte(value)); err != nil {
+				return err
+			}
 		}
 	}
-	return
+
+	// Call before defer to capture error
+	return mwriter.Close()
 }
