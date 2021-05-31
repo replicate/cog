ccd3e9bc3c39ce07de4cf577dc1bd196a6aaac04
Ben Firshman <ben@firshman.co.uk>
2021-05-31 14:10:52 -0700

    Fix Dockerfile cleanup
    If there was an error, it wouldn't get cleaned up.
    
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- pkg/server/queue.go
+++ pkg/server/queue.go
@@ -248,15 +248,15 @@ func (q *BuildQueue) handleJob(job *BuildJob) {
 
 func (q *BuildQueue) buildDockerImage(ctx context.Context, job *BuildJob, logWriter logger.Logger) (string, error) {
 	generator := docker.NewDockerfileGenerator(job.config, job.arch, job.dir)
-	dockerfileContents, err := generator.Generate()
-	if err != nil {
-		return "", fmt.Errorf("Failed to generate Dockerfile for %s: %w", job.arch, err)
-	}
 	defer func() {
 		if err := generator.Cleanup(); err != nil {
 			console.Warnf("Error cleaning up after build: %v", err)
 		}
 	}()
+	dockerfileContents, err := generator.Generate()
+	if err != nil {
+		return "", fmt.Errorf("Failed to generate Dockerfile for %s: %w", job.arch, err)
+	}
 	useGPU := job.config.Environment.BuildRequiresGPU && job.arch == "gpu"
 	uri, err := q.dockerImageBuilder.Build(ctx, job.dir, dockerfileContents, job.name, useGPU, logWriter)
 	if err != nil {
