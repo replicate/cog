1a5177b2288176f94058b444e91e0ebea12ba05c
Ben Firshman <ben@firshman.co.uk>
2021-07-19 10:02:49 -0700

    Remove terminal package
    Back to basics. If we want stuff like this, instead use something
    more mature like Bubble Tea.
    
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,11 +1,8 @@
 package logger
 
 import (
-	"fmt"
-
 	"github.com/replicate/cog/pkg/model"
 	"github.com/replicate/cog/pkg/util/console"
-	"github.com/replicate/cog/pkg/util/terminal"
 )
 
 type Level int
@@ -65,53 +62,4 @@ func (l *ConsoleLogger) WriteError(err error) {
 // TODO(bfirsh): remove
 func (l *ConsoleLogger) WriteVersion(version *model.Version) {
 	console.Infof(l.prefix+"%v", version)
-}
-
-type TerminalLogger struct {
-	ui        terminal.UI
-	stepGroup terminal.StepGroup
-	step      terminal.Step
-}
-
-func NewTerminalLogger(ui terminal.UI) *TerminalLogger {
-	l := &TerminalLogger{ui: ui}
-	l.stepGroup = ui.StepGroup()
-	return l
-}
-
-func (l *TerminalLogger) Info(line string) {
-	l.Infof(line)
-}
-
-func (l *TerminalLogger) Debug(line string) {
-	l.Debugf(line)
-}
-
-func (l *TerminalLogger) Infof(line string, args ...interface{}) {
-	if l.step != nil {
-		l.step.Done()
-	}
-	l.step = l.stepGroup.Add(fmt.Sprintf(line, args...))
-}
-
-func (l *TerminalLogger) Debugf(line string, args ...interface{}) {
-	if l.step != nil {
-		fmt.Fprintf(l.step.TermOutput(), line+"\n", args...)
-	}
-}
-
-func (l *TerminalLogger) WriteStatus(status string, args ...interface{}) {
-	l.Infof("status: "+status, args...)
-}
-
-func (l *TerminalLogger) WriteError(err error) {
-	l.step.Abort()
-}
-
-func (l *TerminalLogger) WriteVersion(version *model.Version) {
-	l.Infof("version: %v", version)
-}
-
-func (l *TerminalLogger) Done() {
-	l.step.Done()
 }
