20cd4bd51e713819d18ce940fa76aae7237a3b19
Ben Firshman <ben@firshman.co.uk>
2021-07-22 16:33:12 -0700

    Remove Dockerfile section labels
    This was creating lots of unnecessary layers, which slows
    builds/pushes.
    
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- pkg/dockerfile/generator_test.go
+++ pkg/dockerfile/generator_test.go
@@ -11,14 +11,12 @@ import (
 )
 
 func testInstallCog(generatedPaths []string) string {
-	return `RUN ### --> Installing Cog
-COPY .cog/tmp/cog-0.0.1.dev-py3-none-any.whl /tmp/cog-0.0.1.dev-py3-none-any.whl
+	return `COPY .cog/tmp/cog-0.0.1.dev-py3-none-any.whl /tmp/cog-0.0.1.dev-py3-none-any.whl
 RUN pip install /tmp/cog-0.0.1.dev-py3-none-any.whl`
 }
 
 func testInstallPython(version string) string {
-	return fmt.Sprintf(`RUN ### --> Installing Python prerequisites
-ENV PATH="/root/.pyenv/shims:/root/.pyenv/bin:$PATH"
+	return fmt.Sprintf(`ENV PATH="/root/.pyenv/shims:/root/.pyenv/bin:$PATH"
 RUN apt-get update -q && apt-get install -qy --no-install-recommends \
 	make \
 	build-essential \
@@ -40,7 +38,6 @@ RUN apt-get update -q && apt-get install -qy --no-install-recommends \
 	git \
 	ca-certificates \
 	&& rm -rf /var/lib/apt/lists/*
-RUN ### --> Installing Python 3.8
 RUN curl https://pyenv.run | bash && \
 	git clone https://github.com/momo-lab/pyenv-install-latest.git "$(pyenv root)"/plugins/pyenv-install-latest && \
 	pyenv install-latest "%s" && \
@@ -69,7 +66,6 @@ ENV LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu
 ` + testInstallCog(gen.generatedPaths) + `
 WORKDIR /src
 CMD ["python", "-m", "cog.server.http"]
-RUN ### --> Copying code
 COPY . /src`
 
 	gen = DockerfileGenerator{Config: conf, Arch: "gpu", Dir: tmpDir}
@@ -83,7 +79,6 @@ ENV LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu
 ` + testInstallPython("3.8") + testInstallCog(gen.generatedPaths) + `
 WORKDIR /src
 CMD ["python", "-m", "cog.server.http"]
-RUN ### --> Copying code
 COPY . /src`
 
 	require.Equal(t, expectedCPU, actualCPU)
@@ -117,16 +112,12 @@ ENV DEBIAN_FRONTEND=noninteractive
 ENV PYTHONUNBUFFERED=1
 ENV LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu
 ` + testInstallCog(gen.generatedPaths) + `
-RUN ### --> Installing system packages
 RUN apt-get update -qq && apt-get install -qy ffmpeg cowsay && rm -rf /var/lib/apt/lists/*
-RUN ### --> Installing Python requirements
 COPY my-requirements.txt /tmp/requirements.txt
 RUN pip install -r /tmp/requirements.txt && rm /tmp/requirements.txt
-RUN ### --> Installing Python packages
 RUN pip install -f https://download.pytorch.org/whl/torch_stable.html   torch==1.5.1+cpu pandas==1.2.0.12
 WORKDIR /src
 CMD ["python", "-m", "cog.server.http"]
-RUN ### --> Copying code
 COPY . /src`
 
 	gen = DockerfileGenerator{Config: conf, Arch: "gpu", Dir: tmpDir}
@@ -139,16 +130,12 @@ ENV PYTHONUNBUFFERED=1
 ENV LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu
 ` + testInstallPython("3.8") +
 		testInstallCog(gen.generatedPaths) + `
-RUN ### --> Installing system packages
 RUN apt-get update -qq && apt-get install -qy ffmpeg cowsay && rm -rf /var/lib/apt/lists/*
-RUN ### --> Installing Python requirements
 COPY my-requirements.txt /tmp/requirements.txt
 RUN pip install -r /tmp/requirements.txt && rm /tmp/requirements.txt
-RUN ### --> Installing Python packages
 RUN pip install   torch==1.5.1 pandas==1.2.0.12
 WORKDIR /src
 CMD ["python", "-m", "cog.server.http"]
-RUN ### --> Copying code
 COPY . /src`
 
 	require.Equal(t, expectedCPU, actualCPU)
