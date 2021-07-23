7236ab4eba3ec33ed46342af1dfd8501dd8939af
Ben Firshman <ben@firshman.co.uk>
2021-07-22 19:24:21 -0700

    Display log output for predict
    This still needs cleaning up. #169
    
    Closes #137
    Closes #154
    
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

---
+++ pkg/docker/logs.go
@@ -0,0 +1,14 @@
+package docker
+
+import (
+	"io"
+	"os"
+	"os/exec"
+)
+
+func ContainerLogsFollow(containerID string, out io.Writer) error {
+	cmd := exec.Command("docker", "container", "logs", "--follow", containerID)
+	cmd.Env = os.Environ()
+	cmd.Stdout = out
+	return cmd.Run()
+}
