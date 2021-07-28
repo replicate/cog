6049fe10ee0fb936e3e7f84245b21ade285852d2
Ben Firshman <ben@firshman.co.uk>
2021-07-27 17:02:48 -0700

    Add test for predict with just an image
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- test-integration/test_integration/test_predict.py
+++ test-integration/test_integration/test_predict.py
@@ -1,5 +1,7 @@
 import subprocess
 
+from .util import random_string
+
 
 def test_predict(tmpdir_factory):
     tmpdir = tmpdir_factory.mktemp("project")
@@ -32,5 +34,45 @@ environment:
 
 
 def test_predict_with_existing_image(tmpdir_factory):
-    # TODO: when there is a build command
-    pass
+    image_name = "cog-test-" + random_string(10)
+    try:
+        tmpdir = tmpdir_factory.mktemp("project")
+        with open(tmpdir / "predict.py", "w") as f:
+            f.write(
+                """
+import cog
+
+class Model(cog.Model):
+    def setup(self):
+        pass
+
+    @cog.input("input", type=str)
+    def predict(self, input):
+        return "hello " + input
+        """
+            )
+        with open(tmpdir / "cog.yaml", "w") as f:
+            cog_yaml = """
+model: "predict.py:Model"
+environment:
+python: "3.8"
+            """
+            f.write(cog_yaml)
+
+        subprocess.run(
+            ["cog", "build", "-t", image_name],
+            cwd=tmpdir,
+            check=True,
+        )
+
+        # Run in another directory to ensure it doesn't use cog.yaml
+        another_directory = tmpdir_factory.mktemp("project")
+        result = subprocess.run(
+            ["cog", "predict", image_name, "-i", "world"],
+            cwd=another_directory,
+            check=True,
+            capture_output=True,
+        )
+        assert b"hello world" in result.stdout
+    finally:
+        subprocess.run(["docker", "rmi", image_name], check=True)
