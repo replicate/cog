7c1d945d73c5482ebd7e84fe808973cae5156dbe
Zeke Sikelianos <zeke@sikelianos.com>
2021-09-20 11:43:46 -0700

    add more http generator tests
    Signed-off-by: Zeke Sikelianos <zeke@sikelianos.com>

--- python/cog_test.py
+++ python/cog_test.py
@@ -352,22 +352,6 @@ def test_default_path_input():
     assert resp.status_code == 200
     assert resp.data == b"noneee"
 
-def test_yielding_strings():
-    class Predictor(cog.Predictor):
-        def setup(self):
-            pass
-
-        def predict(self):
-            predictions = ["foo", "bar", "baz"]
-            for prediction in predictions:
-                yield prediction
-
-    client = make_client(Predictor())
-    resp = client.post("/predict")
-    assert resp.status_code == 200
-    assert resp.content_type == "text/plain; charset=utf-8"
-    assert resp.data == b"baz"
-
 def test_path_output_str():
     class Predictor(cog.Predictor):
         def setup(self):
@@ -491,6 +475,66 @@ def test_type_signature():
         ]
     }
 
+def test_yielding_strings_from_generator_predictors():
+    class Predictor(cog.Predictor):
+        def setup(self):
+            pass
+
+        def predict(self):
+            predictions = ["foo", "bar", "baz"]
+            for prediction in predictions:
+                yield prediction
+
+    client = make_client(Predictor())
+    resp = client.post("/predict")
+    assert resp.status_code == 200
+    assert resp.content_type == "text/plain; charset=utf-8"
+    assert resp.data == b"baz"
+
+def test_yielding_json_from_generator_predictors():
+    class Predictor(cog.Predictor):
+        def setup(self):
+            pass
+
+        def predict(self):
+            predictions = [
+                {"meaning_of_life": 40}, 
+                {"meaning_of_life": 41},
+                {"meaning_of_life": 42}
+            ]
+            for prediction in predictions:
+                yield prediction
+
+    client = make_client(Predictor())
+    resp = client.post("/predict")
+    assert resp.status_code == 200
+    assert resp.content_type == "application/json"
+    assert resp.data == b'{"meaning_of_life": 42}'
+
+
+def test_yielding_files_from_generator_predictors():
+    class Predictor(cog.Predictor):
+        def setup(self):
+            pass
+
+        def predict(self):
+            colors = ["red", "blue", "yellow"]
+            for i, color in enumerate(colors):
+                temp_dir = tempfile.mkdtemp()
+                temp_path = os.path.join(temp_dir, f"prediction-{i}.bmp")
+                img = Image.new("RGB", (255, 255), color)
+                img.save(temp_path)
+                yield Path(temp_path)
+
+    client = make_client(Predictor())
+    resp = client.post("/predict")
+    
+    assert resp.status_code == 200
+    # need both image/bmp and image/x-ms-bmp until https://bugs.python.org/issue44211 is fixed
+    assert resp.content_type in ["image/bmp", "image/x-ms-bmp"]
+    image = Image.open(io.BytesIO(resp.data))
+    image_color = Image.Image.getcolors(image)[0][1]
+    assert image_color == (255, 255, 0) # yellow
 
 def test_timing():
     class PredictorSlow(cog.Predictor):
