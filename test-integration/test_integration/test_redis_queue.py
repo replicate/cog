d57c29ed16da7bf18b99aa13c670c4527353ee3c
Ben Firshman <ben@firshman.co.uk>
2021-09-21 16:23:59 -0700

    Add "failed" status to redis responses
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- test-integration/test_integration/test_redis_queue.py
+++ test-integration/test_integration/test_redis_queue.py
@@ -18,6 +18,7 @@ from .util import (
     wait_for_port,
 )
 
+
 def test_queue_worker_yielding(docker_image, redis_port, request):
     project_dir = Path(__file__).parent / "fixtures/yielding-project"
     subprocess.run(["cog", "build", "-t", docker_image], check=True, cwd=project_dir)
@@ -80,7 +81,68 @@ def test_queue_worker_yielding(docker_image, redis_port, request):
 
         response = json.loads(redis_client.brpop(response_queue_name)[1])
         assert response == {"value": "baz", "status": "success"}
-        
+
+        response = redis_client.rpop(response_queue_name)
+        assert response == None
+
+
+def test_queue_worker_error(docker_image, redis_port, request):
+    project_dir = Path(__file__).parent / "fixtures/failing-project"
+    subprocess.run(["cog", "build", "-t", docker_image], check=True, cwd=project_dir)
+
+    input_queue = multiprocessing.Queue()
+    output_queue = multiprocessing.Queue()
+    controller_port = find_free_port()
+    local_ip = get_local_ip()
+    upload_url = f"http://{local_ip}:{controller_port}/upload"
+    redis_host = local_ip
+    worker_name = "test-worker"
+    predict_queue_name = "predict-queue"
+    response_queue_name = "response-queue"
+
+    redis_client = redis.Redis(host=redis_host, port=redis_port, db=0)
+
+    with queue_controller(
+        input_queue, output_queue, controller_port, request
+    ), docker_run(
+        image=docker_image,
+        interactive=True,
+        command=[
+            "python",
+            "-m",
+            "cog.server.redis_queue",
+            redis_host,
+            str(redis_port),
+            predict_queue_name,
+            upload_url,
+            worker_name,
+            "model_id",
+            "logs",
+        ],
+    ):
+        redis_client.xgroup_create(
+            mkstream=True, groupname=predict_queue_name, name=predict_queue_name, id="$"
+        )
+
+        predict_id = random_string(10)
+        redis_client.xadd(
+            name=predict_queue_name,
+            fields={
+                "value": json.dumps(
+                    {
+                        "id": predict_id,
+                        "inputs": {
+                            "text": {"value": "bar"},
+                        },
+                        "response_queue": response_queue_name,
+                    }
+                ),
+            },
+        )
+
+        response = json.loads(redis_client.brpop(response_queue_name, timeout=10)[1])
+        assert response == {"status": "failed", "error": "over budget"}
+
         response = redis_client.rpop(response_queue_name)
         assert response == None
 
