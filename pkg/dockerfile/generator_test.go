961ccfa82ae02e67cb19ba2397df3474a564a0b0
Michael Floering <michaelfloering@gmail.com>
2021-12-14 20:46:22 -0800

    Judgment call - maybe /src/.cache is clearer
    Signed-off-by: Michael Floering <michaelfloering@gmail.com>

--- pkg/dockerfile/generator_test.go
+++ pkg/dockerfile/generator_test.go
@@ -67,7 +67,7 @@ FROM python:3.8
 ENV DEBIAN_FRONTEND=noninteractive \
 	PYTHONUNBUFFERED=1 \
 	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu:/usr/local/nvidia/lib64:/usr/local/nvidia/bin
-ENV XDG_CACHE_HOME=/src/cog_cache_home
+ENV XDG_CACHE_HOME=/src/.cache
 ` + testInstallCog(gen.relativeTmpDir) + `
 WORKDIR /src
 CMD ["python", "-m", "cog.server.http"]
@@ -134,7 +134,7 @@ FROM python:3.8
 ENV DEBIAN_FRONTEND=noninteractive \
 	PYTHONUNBUFFERED=1 \
 	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu:/usr/local/nvidia/lib64:/usr/local/nvidia/bin
-ENV XDG_CACHE_HOME=/src/cog_cache_home
+ENV XDG_CACHE_HOME=/src/.cache
 ` + testInstallCog(gen.relativeTmpDir) + `
 WORKDIR /src
 CMD ["python", "-m", "cog.server.http"]
@@ -206,7 +206,7 @@ FROM nvidia/cuda:11.2.0-cudnn8-devel-ubuntu20.04
 ENV DEBIAN_FRONTEND=noninteractive \
 	PYTHONUNBUFFERED=1 \
 	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu:/usr/local/nvidia/lib64:/usr/local/nvidia/bin
-ENV XDG_CACHE_HOME=/src/cog_cache_home
+ENV XDG_CACHE_HOME=/src/.cache
 ` + testInstallPython("3.8") + testInstallCog(gen.relativeTmpDir) + `
 WORKDIR /src
 CMD ["python", "-m", "cog.server.http"]
@@ -246,7 +246,7 @@ FROM python:3.8
 ENV DEBIAN_FRONTEND=noninteractive \
 	PYTHONUNBUFFERED=1 \
 	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu:/usr/local/nvidia/lib64:/usr/local/nvidia/bin
-ENV XDG_CACHE_HOME=/src/cog_cache_home
+ENV XDG_CACHE_HOME=/src/.cache
 ` + testInstallCog(gen.relativeTmpDir) + `
 RUN --mount=type=cache,target=/var/cache/apt apt-get update -qq && apt-get install -qqy ffmpeg cowsay && rm -rf /var/lib/apt/lists/*
 COPY my-requirements.txt /tmp/requirements.txt
@@ -290,7 +290,7 @@ FROM nvidia/cuda:10.2-cudnn8-devel-ubuntu18.04
 ENV DEBIAN_FRONTEND=noninteractive \
 	PYTHONUNBUFFERED=1 \
 	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu:/usr/local/nvidia/lib64:/usr/local/nvidia/bin
-ENV XDG_CACHE_HOME=/src/cog_cache_home
+ENV XDG_CACHE_HOME=/src/.cache
 ` + testInstallPython("3.8") +
 		testInstallCog(gen.relativeTmpDir) + `
 RUN --mount=type=cache,target=/var/cache/apt apt-get update -qq && apt-get install -qqy ffmpeg cowsay && rm -rf /var/lib/apt/lists/*
@@ -330,7 +330,7 @@ FROM python:3.8
 ENV DEBIAN_FRONTEND=noninteractive \
 	PYTHONUNBUFFERED=1 \
 	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu:/usr/local/nvidia/lib64:/usr/local/nvidia/bin
-ENV XDG_CACHE_HOME=/src/cog_cache_home
+ENV XDG_CACHE_HOME=/src/.cache
 ` + testInstallCog(gen.relativeTmpDir) + `
 RUN --mount=type=cache,target=/var/cache/apt apt-get update -qq && apt-get install -qqy cowsay && rm -rf /var/lib/apt/lists/*
 RUN cowsay moo
