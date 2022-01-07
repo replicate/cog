e01e07ef352c9b1f087d5b1c49d0d4d5a9f32337
Ben Firshman <ben@firshman.co.uk>
2022-01-07 11:11:44 -0800

    Clean up build integration tests
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- test-integration/test_integration/test_build.py
+++ test-integration/test_integration/test_build.py
@@ -4,97 +4,75 @@ import subprocess
 from .util import random_string
 
 
-def test_build(tmpdir_factory):
-    tmpdir = tmpdir_factory.mktemp("project")
-    with open(tmpdir / "cog.yaml", "w") as f:
-        cog_yaml = """
-build:
-  python_version: 3.8
-"""
-        f.write(cog_yaml)
-
-    image_name = "cog-test-" + random_string(length=10)
-
-    try:
+def test_build_without_predictor(docker_image):
+    project_dir = Path(__file__).parent / "fixtures/no-predictor-project"
+    subprocess.run(
+        ["cog", "build", "-t", docker_image],
+        cwd=project_dir,
+        check=True,
+    )
+    assert docker_image in str(
+        subprocess.run(["docker", "images"], capture_output=True, check=True).stdout
+    )
+    image = json.loads(
         subprocess.run(
-            ["cog", "build", "-t", image_name],
-            cwd=tmpdir,
+            ["docker", "image", "inspect", docker_image],
+            capture_output=True,
             check=True,
-        )
-        assert image_name in str(
-            subprocess.run(["docker", "images"], capture_output=True).stdout
-        )
-        image = json.loads(
-            subprocess.run(
-                ["docker", "image", "inspect", image_name], capture_output=True
-            ).stdout
-        )
-        labels = image[0]["Config"]["Labels"]
-        assert len(labels["org.cogmodel.cog_version"]) > 0
-        print(labels["org.cogmodel.config"])
-        assert json.loads(image[0]["Config"]["Labels"]["org.cogmodel.config"]) == {
-            "build": {"python_version": "3.8"}
-        }
-        assert (
-            json.loads(image[0]["Config"]["Labels"]["org.cogmodel.type_signature"])
-            == {}
-        )
-    finally:
-        subprocess.run(["docker", "rmi", image_name], check=False)
+        ).stdout
+    )
+    labels = image[0]["Config"]["Labels"]
+    assert len(labels["org.cogmodel.cog_version"]) > 0
+    assert json.loads(labels["org.cogmodel.config"]) == {
+        "build": {"python_version": "3.8"}
+    }
+    assert json.loads(labels["org.cogmodel.type_signature"]) == {}
 
 
-def test_build_image_option(tmpdir_factory):
+def test_build_names_uses_image_option_in_cog_yaml(tmpdir_factory, docker_image):
     tmpdir = tmpdir_factory.mktemp("project")
-    image_name = "cog-test-" + random_string(length=10)
     with open(tmpdir / "cog.yaml", "w") as f:
         cog_yaml = f"""
-image: {image_name}
+image: {docker_image}
 build:
   python_version: 3.8
 """
         f.write(cog_yaml)
 
-    try:
-        subprocess.run(
-            ["cog", "build"],
-            cwd=tmpdir,
-            check=True,
-        )
-        assert image_name in str(
-            subprocess.run(["docker", "images"], capture_output=True).stdout
-        )
-    finally:
-        subprocess.run(["docker", "rmi", image_name], check=False)
+    subprocess.run(
+        ["cog", "build"],
+        cwd=tmpdir,
+        check=True,
+    )
+    assert docker_image in str(
+        subprocess.run(["docker", "images"], capture_output=True, check=True).stdout
+    )
 
 
-def test_build_with_model():
+def test_build_with_model(docker_image):
     project_dir = Path(__file__).parent / "fixtures/file-project"
-    image_name = "cog-test-" + random_string(length=10)
-    try:
+    subprocess.run(
+        ["cog", "build", "-t", docker_image],
+        cwd=project_dir,
+        check=True,
+    )
+    image = json.loads(
         subprocess.run(
-            ["cog", "build", "-t", image_name],
-            cwd=project_dir,
+            ["docker", "image", "inspect", docker_image],
+            capture_output=True,
             check=True,
-        )
-        image = json.loads(
-            subprocess.run(
-                ["docker", "image", "inspect", image_name], capture_output=True
-            ).stdout
-        )
-        labels = image[0]["Config"]["Labels"]
-        assert json.loads(
-            image[0]["Config"]["Labels"]["org.cogmodel.type_signature"]
-        ) == {
-            "inputs": [
-                {"name": "text", "type": "str"},
-                {"name": "path", "type": "Path"},
-            ]
-        }
-    finally:
-        subprocess.run(["docker", "rmi", image_name], check=False)
+        ).stdout
+    )
+    labels = image[0]["Config"]["Labels"]
+    assert json.loads(labels["org.cogmodel.type_signature"]) == {
+        "inputs": [
+            {"name": "text", "type": "str"},
+            {"name": "path", "type": "Path"},
+        ]
+    }
 
 
-def test_build_gpu_model_on_cpu(tmpdir_factory):
+def test_build_gpu_model_on_cpu(tmpdir_factory, docker_image):
     tmpdir = tmpdir_factory.mktemp("project")
     with open(tmpdir / "cog.yaml", "w") as f:
         cog_yaml = """
@@ -104,35 +82,29 @@ build:
 """
         f.write(cog_yaml)
 
-    image_name = "cog-test-" + random_string(length=10)
-
-    try:
+    subprocess.run(
+        ["cog", "build", "-t", docker_image],
+        cwd=tmpdir,
+        check=True,
+    )
+    assert docker_image in str(
+        subprocess.run(["docker", "images"], capture_output=True, check=True).stdout
+    )
+    image = json.loads(
         subprocess.run(
-            ["cog", "build", "-t", image_name],
-            cwd=tmpdir,
+            ["docker", "image", "inspect", docker_image],
+            capture_output=True,
             check=True,
-        )
-        assert image_name in str(
-            subprocess.run(["docker", "images"], capture_output=True).stdout
-        )
-        image = json.loads(
-            subprocess.run(
-                ["docker", "image", "inspect", image_name], capture_output=True
-            ).stdout
-        )
-        labels = image[0]["Config"]["Labels"]
-        assert len(labels["org.cogmodel.cog_version"]) > 0
-        assert json.loads(image[0]["Config"]["Labels"]["org.cogmodel.config"]) == {
-            "build": {
-                "python_version": "3.8",
-                "gpu": True,
-                "cuda": "11.2",
-                "cudnn": "8",
-            }
+        ).stdout
+    )
+    labels = image[0]["Config"]["Labels"]
+    assert len(labels["org.cogmodel.cog_version"]) > 0
+    assert json.loads(labels["org.cogmodel.config"]) == {
+        "build": {
+            "python_version": "3.8",
+            "gpu": True,
+            "cuda": "11.2",
+            "cudnn": "8",
         }
-        assert (
-            json.loads(image[0]["Config"]["Labels"]["org.cogmodel.type_signature"])
-            == {}
-        )
-    finally:
-        subprocess.run(["docker", "rmi", image_name], check=False)
+    }
+    assert json.loads(labels["org.cogmodel.type_signature"]) == {}
