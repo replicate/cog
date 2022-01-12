27c63bf24d8f99cb78ad1c9da74d457410327295
Ben Firshman <ben@firshman.co.uk>
2022-01-12 12:05:00 -0800

    Install wheel with pyenv Python
    Not having wheel installed makes installing some packages very slow. We
    noticed dlib particularly.
    
    Docker's Python base image already installs the wheel package, but it is
    not installed when we install Python ourselves with pyenv.
    
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- pkg/dockerfile/generator_test.go
+++ pkg/dockerfile/generator_test.go
@@ -41,7 +41,8 @@ RUN --mount=type=cache,target=/var/cache/apt apt-get update -qq && apt-get insta
 RUN curl https://pyenv.run | bash && \
 	git clone https://github.com/momo-lab/pyenv-install-latest.git "$(pyenv root)"/plugins/pyenv-install-latest && \
 	pyenv install-latest "%s" && \
-	pyenv global $(pyenv install-latest --print "%s")
+	pyenv global $(pyenv install-latest --print "%s") && \
+	pip install "wheel<1"
 `, version, version)
 }
 
