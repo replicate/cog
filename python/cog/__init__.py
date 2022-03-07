9b2a503f68a4e13c1b94201c11fc435c7b5d6f3d
Ben Firshman <ben@firshman.co.uk>
2022-03-07 12:13:33 -0800

    Remove cog.Predictor
    We're doing a backwards incompatible change anyway, and this is actually
    a neat bit of forced compatibility. If you try to use a old Cog model
    with new Cog it will just fail because it can't import `Predictor`,
    instead of failing somewhere deeper in the stack.
    
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- python/cog/__init__.py
+++ python/cog/__init__.py
@@ -3,14 +3,10 @@ from pydantic import BaseModel
 from .predictor import BasePredictor
 from .types import File, Input, Path
 
-# Backwards compatibility. Will be deprecated before 1.0.0.
-Predictor = BasePredictor
-
 __all__ = [
     "BaseModel",
     "BasePredictor",
     "File",
     "Input",
     "Path",
-    "Predictor",
 ]
