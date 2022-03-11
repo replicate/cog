9052ef4babd1958e80799a367d50c935052b78d4
andreasjansson <andreas@replicate.ai>
2022-03-11 15:21:20 -0800

    Add redis test for yielding returning function
    Signed-off-by: andreasjansson <andreas@replicate.ai>

---
+++ test-integration/test_integration/fixtures/yielding-returning-project/predict.py
@@ -0,0 +1,12 @@
+from typing import Iterator
+
+from cog import BasePredictor
+
+
+class Predictor(BasePredictor):
+    def predict(self, text: str) -> Iterator[str]:
+        predictions = ["foo", "bar", "baz"]
+        for prediction in predictions:
+            yield prediction
+
+        return "qux"
--- test-integration/test_integration/test_redis_queue.py
+++ test-integration/test_integration/test_redis_queue.py
@@ -223,6 +223,66 @@ def test_queue_worker_yielding(docker_network, docker_image, redis_client):
         assert response == None
 
 
+def test_queue_worker_yielding_returning(docker_network, docker_image, redis_client):
+    project_dir = Path(__file__).parent / "fixtures/yielding-returning-project"
+    subprocess.run(["cog", "build", "-t", docker_image], check=True, cwd=project_dir)
+
+    with docker_run(
+        image=docker_image,
+        interactive=True,
+        network=docker_network,
+        command=[
+            "python",
+            "-m",
+            "cog.server.redis_queue",
+            "redis",
+            "6379",
+            "predict-queue",
+            "",
+            "test-worker",
+            "model_id",
+            "logs",
+        ],
+    ):
+        redis_client.xgroup_create(
+            mkstream=True, groupname="predict-queue", name="predict-queue", id="$"
+        )
+
+        predict_id = random_string(10)
+        redis_client.xadd(
+            name="predict-queue",
+            fields={
+                "value": json.dumps(
+                    {
+                        "id": predict_id,
+                        "inputs": {
+                            "text": {"value": "bar"},
+                        },
+                        "response_queue": "response-queue",
+                    }
+                ),
+            },
+        )
+
+        response = json.loads(redis_client.brpop("response-queue", timeout=10)[1])
+        assert response == {"value": ["foo"], "status": "processing"}
+
+        response = json.loads(redis_client.brpop("response-queue", timeout=10)[1])
+        assert response == {"value": ["foo", "bar"], "status": "processing"}
+
+        response = json.loads(redis_client.brpop("response-queue", timeout=10)[1])
+        assert response == {"value": ["foo", "bar", "baz"], "status": "processing"}
+
+        response = json.loads(redis_client.brpop("response-queue", timeout=10)[1])
+        assert response == {"value": ["foo", "bar", "baz", "qux"], "status": "processing"}
+
+        response = json.loads(redis_client.brpop("response-queue", timeout=10)[1])
+        assert response == {"value": ["foo", "bar", "baz", "qux"], "status": "success"}
+
+        response = redis_client.rpop("response-queue")
+        assert response == None
+
+
 def test_queue_worker_error(docker_network, docker_image, redis_client):
     project_dir = Path(__file__).parent / "fixtures/failing-project"
     subprocess.run(["cog", "build", "-t", docker_image], check=True, cwd=project_dir)
