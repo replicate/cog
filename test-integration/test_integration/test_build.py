244a8115eea1972c5230a85770382e8ce11060de
Zeke Sikelianos <zeke@sikelianos.com>
2022-03-21 14:56:06 -0700

    fix invalid empty lists from `cog init` (#493)
    * test `cog init` command
    
    Signed-off-by: Zeke Sikelianos <zeke@sikelianos.com>
    
    * fix invalid empty lists from `cog init`
    
    Signed-off-by: Zeke Sikelianos <zeke@sikelianos.com>
    
    * Update test-integration/test_integration/test_build.py
    
    * use tmpdir instead of tmpdir_factory
    
    h/t @andreasjansson 🧙
    
    Signed-off-by: Zeke Sikelianos <zeke@sikelianos.com>

--- test-integration/test_integration/test_build.py
+++ test-integration/test_integration/test_build.py
@@ -29,8 +29,7 @@ def test_build_without_predictor(docker_image):
     assert "org.cogmodel.openapi_schema" not in labels
 
 
-def test_build_names_uses_image_option_in_cog_yaml(tmpdir_factory, docker_image):
-    tmpdir = tmpdir_factory.mktemp("project")
+def test_build_names_uses_image_option_in_cog_yaml(tmpdir, docker_image):
     with open(tmpdir / "cog.yaml", "w") as f:
         cog_yaml = f"""
 image: {docker_image}
@@ -82,8 +81,7 @@ def test_build_with_model(docker_image):
     }
 
 
-def test_build_gpu_model_on_cpu(tmpdir_factory, docker_image):
-    tmpdir = tmpdir_factory.mktemp("project")
+def test_build_gpu_model_on_cpu(tmpdir, docker_image):
     with open(tmpdir / "cog.yaml", "w") as f:
         cog_yaml = """
 build:
@@ -118,3 +116,22 @@ build:
         }
     }
     assert "org.cogmodel.openapi_schema" not in labels
+
+
+def test_build_with_cog_init_templates(tmpdir, docker_image):
+    subprocess.run(
+        ["cog", "init"],
+        cwd=tmpdir,
+        capture_output=True,
+        check=True,
+    )
+
+    build_process = subprocess.run(
+        ["cog", "build", "-t", docker_image],
+        cwd=tmpdir,
+        capture_output=True,
+        check=True,
+    )
+
+    assert build_process.returncode == 0
+    assert "Image built as cog-" in build_process.stderr.decode()
