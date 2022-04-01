45f351aca11ca9487408dd3684395405c164e055
Ben Firshman <ben@firshman.co.uk>
2022-04-01 14:30:31 -0700

    Install pyenv from github (#525)
    This seemed to be timing out in CI for some reason. All
    https://pyenv.run is called this script.
    
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- pkg/dockerfile/generator.go
+++ pkg/dockerfile/generator.go
@@ -173,7 +173,7 @@ RUN --mount=type=cache,target=/var/cache/apt apt-get update -qq && apt-get insta
 	git \
 	ca-certificates \
 	&& rm -rf /var/lib/apt/lists/*
-` + fmt.Sprintf(`RUN curl https://pyenv.run | bash && \
+` + fmt.Sprintf(`RUN curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash && \
 	git clone https://github.com/momo-lab/pyenv-install-latest.git "$(pyenv root)"/plugins/pyenv-install-latest && \
 	pyenv install-latest "%s" && \
 	pyenv global $(pyenv install-latest --print "%s") && \
