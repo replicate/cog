c7166c2442937c7c758090c4460328c8846c7066
Dominic Baggott <dominic.baggott@gmail.com>
2022-04-05 19:14:10 +0100

    Add logs to queueing API responses
    There's no easy way to do this with incremental changes, so one large
    change plus test updates all in one is how we're doing it.
    
    The log capturing code is more or less a copy of redis_log_capture.py.
    It works, but it could probably do with some more thoughtful refactoring
    at some point in the future. Ultimately, we probably want to do away
    with the redis log capture (or, if we need it, refactor it to use this
    new log capturing code).
    
    We're no longer unlinking files that get returned by the prediction.
    That only works for models returning a simple `Path` -- any model that
    returns a complex object including paths won't get cleaned up anyway, so
    this needs revisiting.
    
    A lot of the test changes are self-explanatory (eg adding an empty
    `logs` property). Some of them are less so:
    
     - The behaviour of yielding predictors that yield multiple times in
       quick succession has changed. Previously we'd emit one message per
       yielded output, because we didn't know when the next yield would
       come. Now that we're sending that output to a pipe, we're able to
       take all the output from the pipe before sending a message.
    
       To accomodate that, the `test_queue_worker_yielding` test now expects
       all those yielded outputs in one go.
    
       To preserve a test for actual incremental outputs, the file yielding
       project now sleeps between yields. This also ensures we're continuing
       to test the behaviour of URLs only being created once for each file.
    
     - Logging test looks for the run logs in output, rather than the Redis
       queue. Setup logs are still on the Redis queue (for now?)
    
     - The timeout behaviour has changed slightly with the new
       implementation. Previously, the timeout timer applied only to the
       time the predictor was running. In between yields, whilst we were
       processing files, the timer was suspended. Now the subprocess runs
       continuously in the background and we process files during the
       timeout timer. To accomodate that, the timings in the
       `test_queue_worker_yielding_timeout` test have been tweaked.
    
    The code flow for predictions that timeout or raise errors means that
    `failed` responses end up just having `status` and `error` returned.
    I've left a TODO in the test file to add a test for some desirable new
    functionality: including any `output` and `logs` generated so far with
    the failed response.
    
    Signed-off-by: Dominic Baggott <dominic.baggott@gmail.com>

---
+++ python/cog/server/log_capture.py
@@ -0,0 +1,116 @@
+import sys
+import uuid
+import multiprocessing
+import os
+import contextlib
+
+
+@contextlib.contextmanager
+def capture_log(logs_dest):
+    """
+    Send each line from stdout and stderr to a pipe in addition to the existing
+    output stream.
+    """
+
+    outs = {
+        "stdout": sys.stdout,
+        "stderr": sys.stderr,
+    }
+    pipe_readers = {}
+    pipe_writers = {}
+    old_fds = {}
+    procs = {}
+
+    # done_token is sent to the logging subprocess as a sentinel signal to break
+    # its infinite loop
+    done_token = str(uuid.uuid4())
+
+    for out_name, out_file in outs.items():
+
+        # create a pipe that we'll send to the logging subprocess
+        pipe_readers[out_name], pipe_writers[out_name] = multiprocessing.Pipe(
+            duplex=False
+        )
+
+        # get a handle to the original stdout/stderr file descriptor
+        old_fds[out_name] = os.dup(out_file.fileno())
+
+        # redirect stdout/stderr to the pipe
+        os.dup2(pipe_writers[out_name].fileno(), out_file.fileno())
+
+        # start the logging subprocess as a daemon
+        procs[out_name] = LogProcess(
+            logs_dest=logs_dest,
+            pipe_reader=pipe_readers[out_name],
+            old_out_fd=old_fds[out_name],
+            done_token=done_token,
+        )
+        procs[out_name].daemon = True
+        procs[out_name].start()
+
+    try:
+        # do the work
+        yield
+
+    finally:
+
+        # write done token to exit the logging loop
+        for out_file in outs.values():
+            out_file.write(done_token + "\n")
+
+        for out_name, out_file in outs.items():
+
+            # exit the logging process as gracefully as possible
+            procs[out_name].join(timeout=1.0)
+            procs[out_name].close()
+
+            # clean up pipe
+            pipe_readers[out_name].close()
+            pipe_writers[out_name].close()
+
+            # reset stdout/stderr to original file descriptor
+            os.dup2(old_fds[out_name], out_file.fileno())
+
+            # close the reference to the original file descriptor
+            os.close(old_fds[out_name])
+
+
+class LogProcess(multiprocessing.Process):
+    def __init__(
+        self,
+        logs_dest,
+        pipe_reader,
+        old_out_fd,
+        done_token,
+    ):
+        super(LogProcess, self).__init__()
+        self.logs_dest = logs_dest
+        self.pipe_reader = pipe_reader
+        self.old_out_fd = old_out_fd
+        self.done_token = done_token
+
+    def run(self):
+        # open the original stdout/stderr. we "tee" to both the original
+        # stdout/stderr as well as the redis queue
+        self.old_out = os.fdopen(self.old_out_fd, "w")
+
+        # open pipe reader
+        read_file = os.fdopen(self.pipe_reader.fileno(), "r", 1)
+
+        # infinite loop until we get the done token
+        while True:
+
+            # read a line from the pipe, stripping the newline
+            line = read_file.readline().rstrip()
+
+            # did we get the done token?
+            if line.strip() == self.done_token:
+                break
+
+            # send the logs to the old stream and the logs pipe
+            self.old_out.write(line + "\n")
+            self.logs_dest.send(line)
+
+        # clean up open files
+        self.old_out.close()
+        self.pipe_reader.close()
