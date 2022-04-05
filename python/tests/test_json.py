48dc3e505d36359d106d4959f0f9324de3ca5b02
Ben Firshman <ben@firshman.co.uk>
2022-04-05 14:29:20 -0700

    Enable file and numpy encode_json() tests
    These were half written and commented out for some reason.
    
    Signed-off-by: Ben Firshman <ben@firshman.co.uk>

--- python/tests/test_json.py
+++ python/tests/test_json.py
@@ -3,6 +3,7 @@ import os
 import tempfile
 
 import cog
+from cog.files import upload_file
 from cog.json import encode_json
 import numpy as np
 from pydantic import BaseModel
@@ -19,41 +20,36 @@ def test_encode_json_encodes_pydantic_models():
     }
 
 
-# TODO
-# def test_file():
-#     class Model(BaseModel):
-#         path: cog.Path
+def test_encode_json_uploads_files():
+    class Model(BaseModel):
+        path: cog.Path
 
-#         class Config:
-#             json_encoders = get_json_encoders()
-
-#     temp_dir = tempfile.mkdtemp()
-#     temp_path = os.path.join(temp_dir, "my_file.txt")
-#     with open(temp_path, "w") as fh:
-#         fh.write("file content")
-#     model = Model(path=cog.Path(temp_path))
-#     assert json.loads(model.json()) == {
-#         "path": "data:text/plain;base64,ZmlsZSBjb250ZW50"
-#     }
+    temp_dir = tempfile.mkdtemp()
+    temp_path = os.path.join(temp_dir, "my_file.txt")
+    with open(temp_path, "w") as fh:
+        fh.write("file content")
+    model = Model(path=cog.Path(temp_path))
+    assert encode_json(model, upload_file) == {
+        "path": "data:text/plain;base64,ZmlsZSBjb250ZW50"
+    }
 
 
-# def test_numpy():
-#     class Model(BaseModel):
-#         ndarray: np.ndarray
-#         npfloat: np.float64
-#         npinteger: np.integer
+def test_numpy():
+    class Model(BaseModel):
+        ndarray: np.ndarray
+        npfloat: np.float64
+        npinteger: np.integer
 
-#         class Config:
-#             json_encoders = get_json_encoders()
-#             arbitrary_types_allowed = True
+        class Config:
+            arbitrary_types_allowed = True
 
-#     model = Model(
-#         ndarray=np.array([[1, 2], [3, 4]]),
-#         npfloat=np.float64(1.3),
-#         npinteger=np.int32(5),
-#     )
-#     assert json.loads(model.json()) == {
-#         "ndarray": [[1, 2], [3, 4]],
-#         "npfloat": 1.3,
-#         "npinteger": 5,
-#     }
+    model = Model(
+        ndarray=np.array([[1, 2], [3, 4]]),
+        npfloat=np.float64(1.3),
+        npinteger=np.int32(5),
+    )
+    assert encode_json(model, None) == {
+        "ndarray": [[1, 2], [3, 4]],
+        "npfloat": 1.3,
+        "npinteger": 5,
+    }
