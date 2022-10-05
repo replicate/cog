2587fc1a03456245c44d5215772ad9b71e2dee84
andreasjansson <andreas@replicate.ai>
2022-10-05 18:01:16 +0200

    Mypy fixes
    Signed-off-by: andreasjansson <andreas@replicate.ai>

--- python/cog/server/response_throttler.py
+++ python/cog/server/response_throttler.py
@@ -5,18 +5,18 @@ from ..response import Status
 
 
 class ResponseThrottler:
-    def __init__(self):
-        self.last_sent_response_time = 0
+    def __init__(self) -> None:
+        self.last_sent_response_time = 0.0
         self.response_interval = float(
             os.environ.get("COG_THROTTLE_RESPONSE_INTERVAL", 0)
         )
 
-    def should_send_response(self, response):
+    def should_send_response(self, response: dict) -> bool:
         if Status.is_terminal(response["status"]):
             return True
 
         seconds_since_last_response = time.time() - self.last_sent_response_time
         return seconds_since_last_response >= self.response_interval
 
-    def update_last_sent_response_time(self):
+    def update_last_sent_response_time(self) -> None:
         self.last_sent_response_time = time.time()
