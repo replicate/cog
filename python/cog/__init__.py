d2726e0653266c895aaba836f74ed4600619dbcc
Nick Stenning <nick@whiteink.com>
2022-12-09 18:09:05 +0100

    Configure Cog python package version from git
    This commit allows the Cog python package to derive its own version from
    git metadata and bake that into the installed package.
    
    To do this, we first update the Cog python package over to
    `pyproject.toml` rather than `setup.py` (as is recommended for modern
    packages[1]) and then use setuptools_scm[2] to define the version
    dynamically.
    
    This should mean that versions work correctly:
    
    - on tagged releases
    - in development
    - when installed directly from git
    - when installed from a git archive zip/tarball
    
    [1]: https://pip.pypa.io/en/stable/reference/build-system/pyproject-toml/
    [2]: https://github.com/pypa/setuptools_scm
    
    Signed-off-by: Nick Stenning <nick@whiteink.com>

--- python/cog/__init__.py
+++ python/cog/__init__.py
@@ -3,7 +3,14 @@ from pydantic import BaseModel
 from .predictor import BasePredictor
 from .types import File, Input, Path
 
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0+unknown"
+
+
 __all__ = [
+    "__version__",
     "BaseModel",
     "BasePredictor",
     "File",
