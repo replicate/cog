79851e8d3e24856f0f2936ca71c2636d72a872f7
Dominic Baggott <dominic.baggott@gmail.com>
2022-12-12 12:04:13 +0000

    Add tests for cog.server.runner.predict()
    Inject the `PredictionEventHandler` class so we can mock it under test.
    
    Signed-off-by: Dominic Baggott <dominic.baggott@gmail.com>

--- python/tests/server/test_runner.py
+++ python/tests/server/test_runner.py
@@ -1,9 +1,17 @@
 import os
 import pytest
 import time
+from unittest import mock
 
-from cog.schema import PredictionRequest
-from cog.server.runner import PredictionRunner
+from cog.schema import PredictionRequest, PredictionResponse
+from cog.server.eventtypes import (
+    Done,
+    Heartbeat,
+    Log,
+    PredictionOutput,
+    PredictionOutputType,
+)
+from cog.server.runner import PredictionRunner, predict
 
 
 def _fixture_path(name):
@@ -22,3 +30,68 @@ def test_prediction_runner():
         assert response.output == "done in 0.1 seconds"
     finally:
         runner.shutdown()
+
+
+# list of (events, calls)
+PREDICT_TESTS = [
+    ([Heartbeat()], []),
+    ([Done()], [mock.call.succeeded()]),
+    ([Done(canceled=True)], [mock.call.canceled()]),
+    ([Done(error=True, error_detail="foo")], [mock.call.failed(error="foo")]),
+    ([Log(source="stdout", message="help")], [mock.call.append_logs("help")]),
+    (
+        [PredictionOutputType(multi=False), PredictionOutput(payload="hello world")],
+        [mock.call.set_output("hello world")],
+    ),
+    (
+        [
+            PredictionOutputType(multi=True),
+            PredictionOutput(payload="hello"),
+            PredictionOutput(payload="world"),
+        ],
+        [
+            mock.call.set_output([]),
+            mock.call.append_output("hello"),
+            mock.call.append_output("world"),
+        ],
+    ),
+    (
+        [
+            PredictionOutputType(multi=False),
+            PredictionOutputType(multi=False),
+            PredictionOutput(payload="hello world"),
+        ],
+        [mock.call.failed(error="Predictor returned unexpected output")],
+    ),
+    (
+        [PredictionOutput(payload="hello world"), Done()],
+        [mock.call.failed(error="Predictor returned unexpected output")],
+    ),
+]
+
+
+def fake_worker(events):
+    class FakeWorker:
+        def predict(self, input_):
+            for e in events:
+                yield e
+
+    return FakeWorker()
+
+
+@pytest.mark.parametrize("events,calls", PREDICT_TESTS)
+def test_predict(events, calls):
+    worker = fake_worker(events)
+    request = PredictionRequest(input={"text": "hello"}, foo="bar")
+    event_handler_class = mock.Mock()
+
+    expected_response = PredictionResponse(**request.dict())
+    response = predict(
+        worker=worker, request=request, event_handler_class=event_handler_class
+    )
+    assert response == expected_response
+
+    event_handler_class.assert_called_once_with(expected_response)
+
+    event_handler = event_handler_class.return_value
+    assert event_handler.method_calls == calls
