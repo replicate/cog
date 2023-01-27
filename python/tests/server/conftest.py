85b0aa8eb132e83479d26e257d09392acbcc5e85
Nick Stenning <nick@whiteink.com>
2023-01-27 13:12:39 +0100

    Fix tests for python 3.7
    mock.call_args didn't acquire "args" and "kwargs" properties until 3.8,
    so this commit reworks the prediction tracker tests to use match
    instead. (And thus moves match to a top-level fixture rather than
    something specific to the server package.)
    
    Signed-off-by: Nick Stenning <nick@whiteink.com>

--- python/tests/server/conftest.py
+++ python/tests/server/conftest.py
@@ -7,20 +7,6 @@ import pytest
 from cog.server.http import create_app
 
 
-class match:
-    def __init__(self, pattern):
-        self.pattern = pattern
-
-    def __eq__(self, other):
-        if not isinstance(other, dict):
-            return self.pattern == other
-        minimal = {k: other[k] for k in self.pattern.keys() if k in other}
-        return self.pattern == minimal
-
-    def __repr__(self):
-        return f"match({repr(self.pattern)})"
-
-
 def _fixture_path(name):
     test_dir = os.path.dirname(os.path.realpath(__file__))
     return os.path.join(test_dir, f"fixtures/{name}.py") + ":Predictor"
