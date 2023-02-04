7f424060ebb2da6d294c304ae4676f1f23f103e9
abhi12-ayalur <abhinav.ayalur@gmail.com>
2023-02-04 21:11:43 +0000

    fix: pydantic at end

--- pkg/dockerfile/generator.go
+++ pkg/dockerfile/generator.go
@@ -97,12 +97,12 @@ func (g *Generator) GenerateBase() (string, error) {
 		g.preamble(),
 		installPython,
 		g.installCython(),
-		g.installPydanticNoBinary(),
 		installCog,
 		aptInstalls,
 		pythonRequirements,
 		pipInstalls,
 		g.installSieve(),
+		g.installPydanticNoBinary(),
 		run,
 		`WORKDIR /src`,
 		`EXPOSE 5000`,
@@ -211,7 +211,7 @@ RUN --mount=type=cache,target=/root/.cache/pip pip install /tmp/%s`, path.Join(g
 }
 
 func (g *Generator) installPydanticNoBinary() string {
-	return "RUN --mount=type=cache,target=/root/.cache/pip pip install pydantic --no-binary :all:"
+	return "RUN --mount=type=cache,target=/root/.cache/pip pip uninstall pydantic && --mount=type=cache,target=/root/.cache/pip pip install pydantic --no-binary :all:"
 }
 
 func (g *Generator) installCython() string {
