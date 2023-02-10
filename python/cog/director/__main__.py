39b6abaaa65a10c3c216b9fee3e7b1b5a0b9b35c
Simon Eskildsen <sirup@sirupsen.com>
2023-02-10 09:17:53 -0500

    director: change default trace exporter to http
    Signed-off-by: Simon Eskildsen <sirup@sirupsen.com>

--- python/cog/director/__main__.py
+++ python/cog/director/__main__.py
@@ -9,7 +9,7 @@ from typing import Any
 import structlog
 import uvicorn
 from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
