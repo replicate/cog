2df5d59403295663a571a189dae49ca17bfb77a0
Nick Stenning <nick@whiteink.com>
2023-04-10 20:38:12 +0200

    Remove unused fixtures
    These fixtures were only used by integration tests that have now moved
    to the director codebase.
    
    Signed-off-by: Nick Stenning <nick@whiteink.com>

--- test-integration/test_integration/fixtures/timeout-project/predict.py
+++
@@ -1,9 +0,0 @@
-import time
-
-from cog import BasePredictor
-
-
-class Predictor(BasePredictor):
-    def predict(self, sleep_time: float) -> str:
-        time.sleep(sleep_time)
-        return f"it worked after {sleep_time} seconds!"
--- test-integration/test_integration/fixtures/train-project/predict.py
+++
@@ -1,9 +0,0 @@
-from cog import BasePredictor, File
-
-
-class Predictor(BasePredictor):
-    def setup(self, weights: File):
-        self.text = weights.read()
-
-    def predict(self) -> str:
-        return f"hello {self.text}"
