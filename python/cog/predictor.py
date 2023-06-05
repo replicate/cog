1d0c5a05386b171c0cb7ff9b29bd232c38a02677
Mattt <mattt@replicate.com>
2023-06-05 05:15:09 -0700

    Update Python version requirements (#1020)
    * Test Python 3.8, 3.9, 3.10, and 3.11
    
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>
    
    * Require Python>=3.8
    
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>
    
    * Import functions from typing instead of typing_extensions
    
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>
    
    * Use unlink(missing_ok=True)
    
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>
    
    ---------
    
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>

--- python/cog/predictor.py
+++ python/cog/predictor.py
@@ -8,10 +8,20 @@ import os.path
 from pathlib import Path
 from pydantic import create_model, BaseModel, Field
 from pydantic.fields import FieldInfo
-from typing import Any, Callable, Dict, List, Optional, Type, Union
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Type,
+    Union,
+    get_origin,
+    get_args,
+)
 
-# Added in Python 3.8. Can be from typing if we drop support for <3.8.
-from typing_extensions import get_origin, get_args, Annotated
+# Added in Python 3.9. Can be from typing if we drop support for <3.9
+from typing_extensions import Annotated
 import yaml
 
 from .errors import ConfigDoesNotExist, PredictorNotSet
@@ -179,9 +189,7 @@ class BaseInput(BaseModel):
             # Note this is pathlib.Path, which cog.Path is a subclass of. A pathlib.Path object shouldn't make its way here,
             # but both have an unlink() method, so may as well be safe.
             elif isinstance(value, Path):
-                # This could be missing_ok=True when we drop support for Python 3.7
-                if value.exists():
-                    value.unlink()
+                value.unlink(missing_ok=True)
 
 
 def get_predict(predictor):
