c9ae97b0634c461c41facee16b9366431a31f6a2
Mattt Zmuda <mattt@replicate.com>
2023-06-22 13:24:59 -0700

    Remove Pillow dependency
    Generate image from hard-coded BMP bytes
    
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>

--- test-integration/test_integration/fixtures/file-output-project/predict.py
+++ test-integration/test_integration/fixtures/file-output-project/predict.py
@@ -2,13 +2,16 @@ import os
 import tempfile
 
 from cog import BasePredictor, Path
-from PIL import Image
-
 
 class Predictor(BasePredictor):
     def predict(self) -> Path:
         temp_dir = tempfile.mkdtemp()
         temp_path = os.path.join(temp_dir, "prediction.bmp")
-        img = Image.new("RGB", (255, 255), "red")
-        img.save(temp_path)
+
+        # 1x1 black bitmap
+        bmp = bytes.fromhex("424D1E000000000000001A0000000C000000010001000100180000000000")
+
+        with open(temp_path, "wb") as file:
+            file.write(bmp)
+
         return Path(temp_path)
