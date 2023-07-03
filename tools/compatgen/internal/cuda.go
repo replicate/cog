2d63fbe8a510e06c1a68467071d4c343e9f0733e
Mattt Zmuda <mattt@replicate.com>
2023-07-03 12:17:38 -0700

    Refactor compatibility generation
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>

---
+++ tools/compatgen/internal/cuda.go
@@ -0,0 +1,85 @@
+package internal
+
+import (
+	"encoding/json"
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/anaskhan96/soup"
+	"github.com/replicate/cog/pkg/config"
+)
+
+func FetchCUDABaseImages() ([]config.CUDABaseImage, error) {
+	url := "https://hub.docker.com/v2/repositories/nvidia/cuda/tags/?page_size=1000&name=devel-ubuntu&ordering=last_updated"
+	tags, err := fetchCUDABaseImageTags(url)
+	if err != nil {
+		return nil, err
+	}
+
+	images := []config.CUDABaseImage{}
+	for _, tag := range tags {
+		image, err := parseCUDABaseImage(tag)
+		if err != nil {
+			return nil, err
+		}
+		images = append(images, *image)
+	}
+
+	return images, nil
+}
+
+func fetchCUDABaseImageTags(url string) ([]string, error) {
+	tags := []string{}
+
+	resp, err := soup.Get(url)
+	if err != nil {
+		return tags, fmt.Errorf("Failed to download %s: %w", url, err)
+	}
+
+	var results struct {
+		Next    *string
+		Results []struct {
+			Name string `json:"name"`
+		} `json:"results"`
+	}
+	if err := json.Unmarshal([]byte(resp), &results); err != nil {
+		return tags, fmt.Errorf("Failed parse CUDA images json: %w", err)
+	}
+
+	for _, result := range results.Results {
+		tag := result.Name
+		if strings.Contains(tag, "-cudnn") && !strings.HasSuffix(tag, "-rc") {
+			tags = append(tags, tag)
+		}
+	}
+
+	// recursive case for pagination
+	if results.Next != nil {
+		nextURL := *results.Next
+		nextTags, err := fetchCUDABaseImageTags(nextURL)
+		if err != nil {
+			return tags, err
+		}
+		tags = append(tags, nextTags...)
+	}
+
+	sort.Sort(sort.Reverse(sort.StringSlice(tags)))
+
+	return tags, nil
+}
+
+func parseCUDABaseImage(tag string) (*config.CUDABaseImage, error) {
+	parts := strings.Split(tag, "-")
+	if len(parts) != 4 {
+		return nil, fmt.Errorf("Tag must be in the format <cudaVersion>-cudnn<cudnnVersion>-{devel,runtime}-ubuntu<ubuntuVersion>. Invalid tag: %s", tag)
+	}
+
+	return &config.CUDABaseImage{
+		Tag:     tag,
+		CUDA:    parts[0],
+		CuDNN:   strings.Split(parts[1], "cudnn")[1],
+		IsDevel: parts[2] == "devel",
+		Ubuntu:  strings.Split(parts[3], "ubuntu")[1],
+	}, nil
+}
---
+++ tools/compatgen/internal/tensorflow.go
@@ -0,0 +1,100 @@
+package internal
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/anaskhan96/soup"
+
+	"github.com/replicate/cog/pkg/config"
+)
+
+func FetchTensorFlowCompatibilityMatrix() ([]config.TFCompatibility, error) {
+	url := "https://www.tensorflow.org/install/source"
+	resp, err := soup.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to download %s: %w", url, err)
+	}
+
+	doc := soup.HTMLParse(resp)
+	gpuHeading := doc.Find("h4", "id", "gpu")
+	table := gpuHeading.FindNextElementSibling()
+	rows := table.FindAll("tr")
+
+	compats := []config.TFCompatibility{}
+	for _, row := range rows[1:] {
+		cells := row.FindAll("td")
+		gpuPackage, packageVersion := split2(cells[0].Text(), "-")
+		pythonVersions, err := parsePythonVersionsCell(cells[1].Text())
+		if err != nil {
+			return nil, err
+		}
+		cuDNN := cells[4].Text()
+		cuda := cells[5].Text()
+
+		compat := config.TFCompatibility{
+			TF:           packageVersion,
+			TFCPUPackage: "tensorflow==" + packageVersion,
+			TFGPUPackage: gpuPackage + "==" + packageVersion,
+			CUDA:         cuda,
+			CuDNN:        cuDNN,
+			Pythons:      pythonVersions,
+		}
+		compats = append(compats, compat)
+	}
+
+	// sanity check
+	if len(compats) < 21 {
+		return nil, fmt.Errorf("Tensorflow compatibility matrix only had %d rows, has the html changed?", len(compats))
+	}
+
+	return compats, nil
+}
+
+func parsePythonVersionsCell(val string) ([]string, error) {
+	versions := []string{}
+	parts := strings.Split(val, ",")
+	for _, part := range parts {
+		part = strings.TrimSpace(part)
+		if strings.Contains(part, "-") {
+			start, end := split2(part, "-")
+			startMajor, startMinor, err := splitPythonVersion(start)
+			if err != nil {
+				return nil, err
+			}
+			endMajor, endMinor, err := splitPythonVersion(end)
+			if err != nil {
+				return nil, err
+			}
+
+			if startMajor != endMajor {
+				return nil, fmt.Errorf("Invalid start and end minor versions: %d, %d", startMajor, endMajor)
+			}
+			for minor := startMinor; minor <= endMinor; minor++ {
+				versions = append(versions, newVersion(startMajor, minor))
+			}
+		} else {
+			versions = append(versions, part)
+		}
+	}
+	return versions, nil
+}
+
+func newVersion(major int, minor int) string {
+	return fmt.Sprintf("%d.%d", major, minor)
+}
+
+func splitPythonVersion(version string) (major int, minor int, err error) {
+	version = strings.TrimSpace(version)
+	majorStr, minorStr := split2(version, ".")
+	major, err = strconv.Atoi(majorStr)
+	if err != nil {
+		return 0, 0, err
+	}
+	minor, err = strconv.Atoi(minorStr)
+	if err != nil {
+		return 0, 0, err
+	}
+	return major, minor, nil
+}
