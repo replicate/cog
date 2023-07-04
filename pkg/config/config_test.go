6f82024a0e2b159f26c4ffe48cbdbf5dabcfbdab
Mattt Zmuda <mattt@replicate.com>
2023-07-04 06:15:35 -0700

    Re-enable TestValidateAndCompleteCUDAForAllTorch
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -146,29 +146,24 @@ func TestValidateAndCompleteCUDAForAllTF(t *testing.T) {
 	}
 }
 
-// func TestValidateAndCompleteCUDAForAllTorch(t *testing.T) {
-// 	// test that all torch versions fill out cuda
-// 	for _, compat := range TorchCompatibilityMatrix {
-// 		config := &Config{
-// 			Build: &Build{
-// 				GPU:           true,
-// 				PythonVersion: "3.8",
-// 				PythonPackages: []string{
-// 					"torch==" + compat.TorchVersion(),
-// 				},
-// 			},
-// 		}
+func TestValidateAndCompleteCUDAForAllTorch(t *testing.T) {
+	for _, compat := range TorchCompatibilityMatrix {
+		config := &Config{
+			Build: &Build{
+				GPU:           compat.CUDA != nil,
+				PythonVersion: "3.8",
+				PythonPackages: []string{
+					"torch==" + compat.TorchVersion(),
+				},
+			},
+		}
 
-// 		for _, cudaBaseImage := range CUDABaseImages {
-// 			if compat.CUDA == nil || strings.HasPrefix(cudaBaseImage.CUDA, *compat.CUDA) {
-// 				err := config.ValidateAndComplete("")
-// 				require.NoError(t, err)
-// 				require.NotEqual(t, "", config.Build.CUDA)
-// 				require.NotEqual(t, "", config.Build.CuDNN)
-// 			}
-// 		}
-// 	}
-// }
+		err := config.ValidateAndComplete("")
+		require.NoError(t, err)
+		require.NotEqual(t, "", config.Build.CUDA)
+		require.NotEqual(t, "", config.Build.CuDNN)
+	}
+}
 
 func TestValidateAndCompleteCUDAForSelectedTorch(t *testing.T) {
 	for _, tt := range []struct {
