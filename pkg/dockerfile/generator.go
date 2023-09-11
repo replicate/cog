2a1a21e5242b96f46fa1c2c1c7edc748d15f103f
technillogue <technillogue@gmail.com>
2023-09-11 13:39:57 -0400

    add --force flag to ln (#1295)
    Signed-off-by: technillogue <technillogue@gmail.com>

--- pkg/dockerfile/generator.go
+++ pkg/dockerfile/generator.go
@@ -380,7 +380,7 @@ func (g *Generator) pipInstalls() string {
 		return strings.Join(
 			[]string{
 				"COPY --from=deps --link /dep /dep",
-				"RUN ln -s /dep/* $(pyenv prefix)/lib/python*/site-packages",
+				"RUN ln --force -s /dep/* $(pyenv prefix)/lib/python*/site-packages",
 			},
 			"\n")
 	}
