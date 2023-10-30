f36dba8b9458d515b687e218d9445f6b23c16720
Mattt <mattt@replicate.com>
2023-10-30 05:54:22 -0700

    Add support for .glb, .mtl and .obj files (#1353)
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>

--- pkg/util/mime/mime_test.go
+++ pkg/util/mime/mime_test.go
@@ -10,6 +10,7 @@ func TestExtensionByType(t *testing.T) {
 	require.Equal(t, ".txt", ExtensionByType("text/plain"))
 	require.Equal(t, ".jpg", ExtensionByType("image/jpeg"))
 	require.Equal(t, ".png", ExtensionByType("image/png"))
+	require.Equal(t, ".obj", ExtensionByType("model/obj"))
 	require.Equal(t, ".json", ExtensionByType("application/json"))
 	require.Equal(t, "", ExtensionByType("asdfasdf"))
 }
@@ -18,6 +19,7 @@ func TestTypeByExtension(t *testing.T) {
 	require.Equal(t, "text/plain", TypeByExtension(".txt"))
 	require.Equal(t, "image/jpeg", TypeByExtension(".jpg"))
 	require.Equal(t, "image/png", TypeByExtension(".png"))
+	require.Equal(t, "model/obj", TypeByExtension(".obj"))
 	require.Equal(t, "application/json", TypeByExtension(".json"))
 	require.Equal(t, "application/octet-stream", TypeByExtension(".asdfasdf"))
 }
