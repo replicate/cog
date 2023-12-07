98de6b9d3b8982eadd506b1655f101060f6abd91
Nick Stenning <nick@whiteink.com>
2023-12-07 10:32:42 +0100

    Stricter image cleanup
    Ensures that images that we expect to clean up are cleaned up correctly.
    With check=True in the call to `docker rmi ...` we will raise if the
    call fails, and the exception will include the command output.
    
    Signed-off-by: Nick Stenning <nick@whiteink.com>

--- test-integration/test_integration/conftest.py
+++ test-integration/test_integration/conftest.py
@@ -11,7 +11,15 @@ def pytest_sessionstart(session):
 
 
 @pytest.fixture
-def docker_image():
-    image = "cog-test-" + random_string(10)
-    yield image
-    subprocess.run(["docker", "rmi", "-f", image], check=False)
+def docker_image_name():
+    return "cog-test-" + random_string(10)
+
+
+@pytest.fixture
+def docker_image(docker_image_name):
+    yield docker_image_name
+    # We expect the image to exist by this point and will fail if it doesn't.
+    # If you just need a name, use docker_image_name.
+    subprocess.run(
+        ["docker", "rmi", docker_image_name], check=True, capture_output=True
+    )
