8a7e53a531e336568aa8874daa1af4b3795ac948
technillogue <technillogue@gmail.com>
2024-01-30 17:36:48 -0500

    move clients into runner, switch to httpx, move create_event_handler into runner
    Signed-off-by: technillogue <technillogue@gmail.com>

--- python/cog/server/webhooks.py
+++ python/cog/server/webhooks.py
@@ -1,14 +1,13 @@
 import os
-from typing import Any, Callable, Set
+from typing import Any, Awaitable, Callable, Set
 
 import httpx
-import requests
 import structlog
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry  # type: ignore
+from httpx._client import USER_AGENT
 
 from ..schema import Status, WebhookEvent
 from .response_throttler import ResponseThrottler
+from .retry_transport import RetryTransport
 
 log = structlog.get_logger(__name__)
 
@@ -28,7 +27,7 @@ def _get_version() -> str:
         return "unknown"
 
 
-_user_agent = f"cog-worker/{_get_version()}"
+_user_agent = f"cog-worker/{_get_version()} {USER_AGENT}"
 _response_interval = float(os.environ.get("COG_THROTTLE_RESPONSE_INTERVAL", 0.5))
 
 # HACK: signal that we should skip the start webhook when the response interval
@@ -36,38 +35,37 @@ _response_interval = float(os.environ.get("COG_THROTTLE_RESPONSE_INTERVAL", 0.5)
 # are latency sensitive.
 SKIP_START_EVENT = _response_interval < 0.1
 
+WebhookSenderType = Callable[[Any, WebhookEvent], Awaitable[None]]
 
-def webhook_caller_filtered(
+def filtered_caller(
     webhook: str,
     webhook_events_filter: Set[WebhookEvent],
-) -> Callable[[Any, WebhookEvent], None]:
+    default_client: httpx.AsyncClient,
+    retry_client: httpx.AsyncClient,
+) -> WebhookSenderType:
     throttler = ResponseThrottler(response_interval=_response_interval)
 
-    default_session = requests_session()
-    retry_session = requests_session_with_retries()
-
-    def upstream_caller(response: Any) -> None:
-
-    def caller(response: Any, event: WebhookEvent) -> None:
+    async def caller(response: Any, event: WebhookEvent) -> None:
         if event in webhook_events_filter:
             if throttler.should_send_response(response):
                 if Status.is_terminal(response["status"]):
                     # For terminal updates, retry persistently
-                    retry_session.post(webhook, json=response)
+                    await retry_client.post(webhook, json=response)
                 else:
                     # For other requests, don't retry, and ignore any errors
                     try:
-                        default_session.post(webhook, json=response)
-                    except requests.exceptions.RequestException:
-                        log.warn("caught exception while sending webhook", exc_info=True)
+                        await default_client.post(webhook, json=response)
+                    except httpx.RequestError:
+                        log.warn(
+                            "caught exception while sending webhook", exc_info=True
+                        )
                 throttler.update_last_sent_response_time()
 
     return caller
 
 
-
-def client_headers() -> dict:
-    headers = {"user-agent": _user_agent + " " + str(client.headers["user-agent"])}
+def client_headers() -> dict[str, str]:
+    headers = {"user-agent": _user_agent}
     auth_token = os.environ.get("WEBHOOK_AUTH_TOKEN")
     if auth_token:
         headers["authorization"] = "Bearer " + auth_token
@@ -75,10 +73,10 @@ def client_headers() -> dict:
 
 
 def httpx_client() -> httpx.AsyncClient:
-    return httx.AsyncClient(headers=client_headers())
+    return httpx.AsyncClient(headers=client_headers())
 
 
-def httpx_retry_client() -> requests.Session:
+def httpx_retry_client() -> httpx.AsyncClient:
     # This session will retry requests up to 12 times, with exponential
     # backoff. In total it'll try for up to roughly 320 seconds, providing
     # resilience through temporary networking and availability issues.
