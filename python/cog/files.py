79a53efc5092254248c5a0337b60d31b9007ec49
technillogue <technillogue@gmail.com>
2024-01-29 19:43:20 -0500

    inline client code
    Signed-off-by: technillogue <technillogue@gmail.com>

--- python/cog/files.py
+++ python/cog/files.py
@@ -2,7 +2,6 @@ import base64
 import io
 import mimetypes
 import os
-from urllib.parse import urlparse
 
 import requests
 
@@ -30,46 +29,3 @@ def upload_file(fh: io.IOBase, output_file_prefix: str = None) -> str:
         mime_type = "application/octet-stream"
     s = encoded_body.decode("utf-8")
     return f"data:{mime_type};base64,{s}"
-
-
-def guess_filename(obj: io.IOBase) -> str:
-    """Tries to guess the filename of the given object."""
-    name = getattr(obj, "name", "file")
-    return os.path.basename(name)
-
-
-def put_file_to_signed_endpoint(
-    fh: io.IOBase, endpoint: str, client: requests.Session
-) -> str:
-    fh.seek(0)
-
-    filename = guess_filename(fh)
-    content_type, _ = mimetypes.guess_type(filename)
-
-    # set connect timeout to slightly more than a multiple of 3 to avoid
-    # aligning perfectly with TCP retransmission timer
-    connect_timeout = 10
-    read_timeout = 15
-
-    resp = client.put(
-        ensure_trailing_slash(endpoint) + filename,
-        fh,  # type: ignore
-        headers={"Content-type": content_type},
-        timeout=(connect_timeout, read_timeout),
-    )
-    resp.raise_for_status()
-
-    # strip any signing gubbins from the URL
-    final_url = urlparse(resp.url)._replace(query="").geturl()
-
-    return final_url
-
-
-def ensure_trailing_slash(url: str) -> str:
-    """
-    Adds a trailing slash to `url` if not already present, and then returns it.
-    """
-    if url.endswith("/"):
-        return url
-    else:
-        return url + "/"
