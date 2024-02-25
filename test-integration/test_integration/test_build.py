e90120c6bd1e19244dd799bd69b65ca0d58c8993
Dmitri Khokhlov <dkhokhlov@gmail.com>
2024-02-25 12:53:35 -0800

    feat: deprecate python 3.7
    Ref: #1431
    
    Signed-off-by: Dmitri Khokhlov <dkhokhlov@gmail.com>

--- test-integration/test_integration/test_build.py
+++ test-integration/test_integration/test_build.py
@@ -208,3 +208,15 @@ def test_build_with_cog_init_templates(tmpdir, docker_image):
 
     assert build_process.returncode == 0
     assert "Image built as cog-" in build_process.stderr.decode()
+
+
+def test_python_37_depricated(docker_image):
+    def test_build_invalid_schema(docker_image):
+        project_dir = Path(__file__).parent / "fixtures/python_37"
+        build_process = subprocess.run(
+            ["cog", "build", "-t", docker_image],
+            cwd=project_dir,
+            capture_output=True,
+        )
+        assert build_process.returncode > 0
+        assert "ERROR: Package 'cog' requires a different Python" in build_process.stderr.decode()
