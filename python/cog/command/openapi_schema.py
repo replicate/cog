c544fee3357477c7ee12f42a46a2e147c471ff07
Dmitri Khokhlov <dkhokhlov@gmail.com>
2024-03-13 23:45:42 -0700

    fix: invalid components: schema "Output": extra sibling fields: [title]
    ref: https://github.com/replicate/cog/issues/1275
    
    Signed-off-by: Dmitri Khokhlov <dkhokhlov@gmail.com>

--- python/cog/command/openapi_schema.py
+++ python/cog/command/openapi_schema.py
@@ -5,12 +5,33 @@ This prints a JSON object describing the inputs of the model.
 """
 
 import json
+from typing import Any, Dict, List, Union
 
 from ..errors import CogError, ConfigDoesNotExist, PredictorNotSet
 from ..predictor import load_config
 from ..schema import Status
 from ..server.http import create_app
 from ..suppress_output import suppress_output
+
+
+def remove_title_next_to_ref(
+    schema_node: Union[Dict[str, Any], List[Any]],
+) -> Union[Dict[str, Any], List[Any]]:
+    """
+    Recursively remove 'title' from schema components that have a '$ref'.
+    This function addresses a non-compliance issue in FastAPI's OpenAPI schema generation, where
+    'title' fields adjacent to '$ref' fields can cause validation problems with some OpenAPI tools.
+    """
+    if isinstance(schema_node, dict):
+        if "$ref" in schema_node and "title" in schema_node:
+            del schema_node["title"]
+        for _key, value in schema_node.items():
+            remove_title_next_to_ref(value)
+    elif isinstance(schema_node, list):  # type: ignore[reportUnnecessaryIsInstance]
+        for i, item in enumerate(schema_node):
+            schema_node[i] = remove_title_next_to_ref(item)
+    return schema_node
+
 
 if __name__ == "__main__":
     schema = {}
@@ -23,7 +44,7 @@ if __name__ == "__main__":
                 and app.state.setup_result.status == Status.FAILED
             ):
                 raise CogError(app.state.setup_result.logs)
-            schema = app.openapi()
+            schema = remove_title_next_to_ref(app.openapi())
     except (ConfigDoesNotExist, PredictorNotSet):
         # If there is no cog.yaml or 'predict' has not been set, then there is no type signature.
         # Not an error, there just isn't anything.
