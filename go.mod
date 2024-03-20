70fcefc58e8201e1107a64ddcd4c1ddb084c9e17
💔 <shah@priansh.com>
2024-03-20 11:59:56 -0700

    quick and dirty

--- go.mod
+++ go.mod
@@ -29,6 +29,11 @@ require (
 require (
 	4d63.com/gocheckcompilerdirectives v1.2.1 // indirect
 	4d63.com/gochecknoglobals v0.2.1 // indirect
+	cloud.google.com/go v0.112.1 // indirect
+	cloud.google.com/go/compute v1.24.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/iam v1.1.6 // indirect
+	cloud.google.com/go/storage v1.39.1 // indirect
 	github.com/4meepo/tagalign v1.3.3 // indirect
 	github.com/Abirdcfly/dupword v0.0.13 // indirect
 	github.com/Antonboom/errname v0.1.12 // indirect
@@ -40,6 +45,8 @@ require (
 	github.com/GaijinEntertainment/go-exhaustruct/v3 v3.2.0 // indirect
 	github.com/Masterminds/semver v1.5.0 // indirect
 	github.com/OpenPeeDeeP/depguard/v2 v2.2.0 // indirect
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
 	github.com/alecthomas/go-check-sumtype v0.1.4 // indirect
 	github.com/alexkohler/nakedret/v2 v2.0.2 // indirect
 	github.com/alexkohler/prealloc v1.0.0 // indirect
@@ -58,7 +65,7 @@ require (
 	github.com/butuzov/mirror v1.1.0 // indirect
 	github.com/catenacyber/perfsprint v0.6.0 // indirect
 	github.com/ccojocar/zxcvbn-go v1.0.2 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/charithe/durationcheck v0.0.10 // indirect
 	github.com/chavacava/garif v0.1.0 // indirect
 	github.com/curioswitch/go-reassign v0.2.0 // indirect
@@ -73,11 +80,14 @@ require (
 	github.com/ettle/strcase v0.2.0 // indirect
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/fatih/structtag v1.2.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/firefart/nonamedreturns v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/fzipp/gocyclo v0.6.0 // indirect
 	github.com/ghostiam/protogetter v0.3.4 // indirect
 	github.com/go-critic/go-critic v0.11.1 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-openapi/jsonpointer v0.20.2 // indirect
 	github.com/go-openapi/swag v0.22.8 // indirect
 	github.com/go-toolsmith/astcast v1.1.0 // indirect
@@ -92,6 +102,7 @@ require (
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gofrs/flock v0.8.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golangci/check v0.0.0-20180506172741-cfe4005ccda2 // indirect
 	github.com/golangci/dupl v0.0.0-20180902072040-3e9179ac440a // indirect
@@ -103,7 +114,11 @@ require (
 	github.com/golangci/revgrep v0.5.2 // indirect
 	github.com/golangci/unconvert v0.0.0-20180507085042-28b1c447d1f4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.2 // indirect
 	github.com/gordonklaus/ineffassign v0.1.0 // indirect
 	github.com/gostaticanalysis/analysisutil v0.7.1 // indirect
 	github.com/gostaticanalysis/comment v1.4.2 // indirect
@@ -196,6 +211,7 @@ require (
 	github.com/ultraware/funlen v0.1.0 // indirect
 	github.com/ultraware/whitespace v0.1.0 // indirect
 	github.com/uudashr/gocognit v1.1.2 // indirect
+	github.com/vbauerster/mpb/v7 v7.5.3 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xen0n/gosmopolitan v1.2.2 // indirect
@@ -205,17 +221,32 @@ require (
 	gitlab.com/bosi/decorder v0.4.1 // indirect
 	go-simpler.org/musttag v0.8.0 // indirect
 	go-simpler.org/sloglint v0.4.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.48.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.48.0 // indirect
+	go.opentelemetry.io/otel v1.23.0 // indirect
+	go.opentelemetry.io/otel/metric v1.23.0 // indirect
+	go.opentelemetry.io/otel/trace v1.23.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.24.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/exp v0.0.0-20240103183307-be819d1f06fc // indirect
 	golang.org/x/exp/typeparams v0.0.0-20231219180239-dc181d75b848 // indirect
 	golang.org/x/mod v0.15.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/oauth2 v0.17.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/term v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/api v0.167.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/genproto v0.0.0-20240213162025-012b6fc9bca9 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240304161311-37d4d3c04a78 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240228224816-df926f6c8641 // indirect
+	google.golang.org/grpc v1.62.0 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	honnef.co/go/tools v0.4.6 // indirect
--- pkg/doctor/doctor.go
+++ pkg/doctor/doctor.go
@@ -2,12 +2,20 @@ package doctor
 
 import (
 	"bufio"
+	"context"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
+	"cloud.google.com/go/storage"
 	"github.com/bmatcuk/doublestar/v4"
+	"github.com/vbauerster/mpb/v7"
+	"github.com/vbauerster/mpb/v7/decor"
+	"gopkg.in/yaml.v3"
 
 	w "github.com/replicate/cog/pkg/weights"
 )
@@ -60,7 +68,46 @@ func CheckFiles() error {
 		for _, file := range weightFiles {
 			fmt.Printf("\t\033[32m%s\033[0m\n", file)
 		}
-		fmt.Printf("\nYou can host them in Replicate's File Cache.\n\n")
+		fmt.Printf("\nIf you have a cache key with Replicate, please enter it now. [skip] ")
+		reader := bufio.NewReader(os.Stdin)
+		response, _ := reader.ReadString('\n')
+		response = strings.TrimSpace(response)
+		if response != "" {
+			urls := []string{}
+			for _, file := range weightFiles {
+				url, err := uploadWeights(file, "replicate-weights-wqzt", response)
+				if err != nil {
+					return err
+				}
+				urls = append(urls, url)
+			}
+			err := writeManifest(urls, weightFiles)
+			if err != nil {
+				return err
+			}
+			err = addDockerignoreEntries(weightFiles)
+			if err != nil {
+				return err
+			}
+			err = writePgetPy()
+			if err != nil {
+				return err
+			}
+			err = installPGet()
+			if err != nil {
+				return err
+			}
+
+			fmt.Println("Added all files to cache.")
+			fmt.Print("In your predictor, you can use the following code to download the weights:\n\n")
+			fmt.Println("\tfrom pget import pget_manifest")
+			fmt.Println("\t...")
+			fmt.Print("\tclass Predictor(BasePredictor):\n\n")
+			fmt.Println("\t  def setup(self):")
+			fmt.Println("\t    pget_manifest()")
+			fmt.Println("\t    ...")
+			fmt.Println("\t}")
+		}
 	}
 
 	return nil
@@ -171,5 +218,193 @@ func addDockerignoreEntries(entries []string) error {
 	}
 
 	defer file.Close()
+	return nil
+}
+
+func uploadWeights(localFilePath, bucketName, folderPrefix string) (string, error) {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return "", nil
+	}
+	defer client.Close()
+
+	objectName := fmt.Sprintf("%s/%s", folderPrefix, filepath.Base(localFilePath))
+	bucket := client.Bucket(bucketName)
+	object := bucket.Object(objectName)
+
+	// FIXME: we should be using a mpu with the signed url below
+	/*
+		urlExpiration := time.Now().Add(60 * time.Minute)
+		url, err := storage.SignedURL(bucketName, objectName, &storage.SignedURLOptions{
+			Method:  "PUT",
+			Expires: urlExpiration,
+		})
+		if err != nil {
+			return err
+		}
+	*/
+
+	file, err := os.Open(localFilePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return "", err
+	}
+
+	p := mpb.New(mpb.WithWidth(60), mpb.WithRefreshRate(180*time.Millisecond))
+	bar := p.AddBar(fileInfo.Size(),
+		mpb.PrependDecorators(
+			decor.Name("Uploading: "),
+			decor.CountersKibiByte("% .2f / % .2f"),
+		),
+		mpb.AppendDecorators(
+			decor.Percentage(),
+			decor.EwmaSpeed(decor.UnitKiB, "% .2f", 60),
+		),
+	)
+
+	const chunkSize = 25 * 1024 * 1024 // 25MB chunk size
+	totalChunks := int(fileInfo.Size() / chunkSize)
+	if fileInfo.Size()%chunkSize != 0 {
+		totalChunks++ // Account for last partial chunk
+	}
+
+	chunk := make([]byte, chunkSize)
+	wc := object.NewWriter(ctx)
+	for i := 0; i < totalChunks; i++ {
+		n, err := file.Read(chunk)
+		if err != nil && err != io.EOF {
+			return "", err
+		}
+
+		_, err = wc.Write(chunk[:n])
+		if err != nil {
+			return "", err
+		}
+
+		// Update progress bar after each chunk is uploaded
+		bar.IncrBy(n)
+	}
+	err = wc.Close()
+	if err != nil {
+		return "", err
+	}
+
+	p.Wait() // Wait for the progress bar to finish
+	fmt.Println("Upload completed successfully.")
+	url := fmt.Sprintf("https://weights.replicate.delivery/wqzt/%s", objectName)
+	return url, nil
+}
+
+func writeManifest(urls []string, filepaths []string) error {
+	if len(urls) != len(filepaths) {
+		return fmt.Errorf("urls and filepaths slices must have the same length")
+	}
+
+	file, err := os.Create("manifest.pget")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for i := range urls {
+		line := fmt.Sprintf("%s %s\n", urls[i], filepaths[i])
+		if _, err := file.WriteString(line); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+var PGET_PY = `# Python Utility for PGet ⥥
+# https://github.com/replicate/pget
+# Use this script with a pget manifest file:
+# from pget import pget_manifest
+# pget_manifest('manifest.pget')
+
+# Your manifest file must be in shape:
+# https://example.com/image1.jpg /local/path/to/image1.jpg
+# https://example.com/document.pdf /local/path/to/document.pdf
+# https://example.com/weights.pth /local/path/to/weights.pth
+# ... etc ..
+
+# Read more about pget multifile downloads here:
+# https://github.com/replicate/pget?tab=readme-ov-file#multi-file-mode
+
+import os
+import subprocess
+import time
+
+def pget_manifest(manifest_filename: str='manifest.pget'):
+  start = time.time()
+  with open(manifest_filename, 'r') as f:
+    manifest = f.read()
+
+
+  # ensure directories exist
+  for line in manifest.splitlines():
+    _, path = line.split(" ")
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
+  # download using pget
+  subprocess.check_call(["pget", "multifile", manifest_filename])
+
+  # log metrics
+  timing = time.time() - start
+  print(f"Downloaded weights in {timing} seconds")`
+
+func writePgetPy() error {
+	file, err := os.Create("pget.py")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	if _, err := file.WriteString(PGET_PY); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type BarebonesCog struct {
+	Run []string `yaml:"run,omitempty"`
+}
+
+func installPGet() error {
+	var config BarebonesCog
+
+	// Read the existing cog.yaml file
+	data, err := ioutil.ReadFile("cog.yaml")
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+
+	if err == nil {
+		// File exists, unmarshal its content
+		if err := yaml.Unmarshal(data, &config); err != nil {
+			return err
+		}
+	}
+
+	// Update or set the run block
+	config.Run = []string{"curl -o /usr/local/bin/pget -L \"https://github.com/replicate/pget/releases/latest/download/pget_$(uname -s)_$(uname -m)\"", "chmod +x /usr/local/bin/pget"}
+
+	// Marshal the modified structure back to YAML
+	updatedData, err := yaml.Marshal(&config)
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile("cog.yaml", updatedData, 0o755); err != nil {
+		return err
+	}
+
 	return nil
 }
