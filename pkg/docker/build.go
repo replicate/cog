3cf1e448ca54088d797bc774ccc4b7a9f075aae0
Mattt Zmuda <mattt@replicate.com>
2024-04-17 04:49:31 -0700

    Always pass --load flag to docker buildx build command
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>

--- pkg/docker/build.go
+++ pkg/docker/build.go
@@ -17,12 +17,12 @@ func Build(dir, dockerfile, imageName string, secrets []string, noCache bool, pr
 	var args []string
 
 	args = append(args,
-		"buildx", "build",
+		"buildx", "build", "--load",
 	)
 
 	if util.IsAppleSiliconMac(runtime.GOOS, runtime.GOARCH) {
 		// Fixes "WARNING: The requested image's platform (linux/amd64) does not match the detected host platform (linux/arm64/v8) and no specific platform was requested"
-		args = append(args, "--platform", "linux/amd64", "--load")
+		args = append(args, "--platform", "linux/amd64")
 	}
 
 	for _, secret := range secrets {
