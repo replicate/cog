f76a158ac7e99d41a0815d4260c65a38039635ea
Mattt Zmuda <mattt@replicate.com>
2024-04-17 06:27:18 -0700

    Fix warning: Condition will always evaluate to False since the types PredictionRequest and None have no overlap
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>

--- python/cog/server/http.py
+++ python/cog/server/http.py
@@ -318,7 +318,7 @@ def create_app(
         return _predict(request=request, respond_async=respond_async)
 
     def _predict(
-        *, request: PredictionRequest, respond_async: bool = False
+        *, request: Optional[PredictionRequest], respond_async: bool = False
     ) -> Response:
         # [compat] If no body is supplied, assume that this model can be run
         # with empty input. This will throw a ValidationError if that's not
