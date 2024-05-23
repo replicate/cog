34f469e255c58862f9ffbe5bf5b04eeb6c46b613
Mattt Zmuda <mattt@replicate.com>
2024-05-23 09:55:13 -0700

    Skip numpy tests for pydantic v2
    Signed-off-by: Mattt Zmuda <mattt@replicate.com>

--- python/tests/test_json.py
+++ python/tests/test_json.py
@@ -53,20 +53,21 @@ def test_upload_files():
     }
 
 
-def test_numpy():
-    class Model(pydantic.BaseModel):
-        ndarray: np.ndarray
-        npfloat: np.float64
-        npinteger: np.integer
-        model_config = ConfigDict(arbitrary_types_allowed=True)
+if not PYDANTIC_V2:
 
-    model = Model(
-        ndarray=np.array([[1, 2], [3, 4]]),
-        npfloat=np.float64(1.3),
-        npinteger=np.int32(5),
-    )
-    assert make_encodeable(model) == {
-        "ndarray": [[1, 2], [3, 4]],
-        "npfloat": 1.3,
-        "npinteger": 5,
-    }
+    def test_numpy():
+        class Model(pydantic.BaseModel):
+            ndarray: np.ndarray
+            npfloat: np.float64
+            npinteger: np.integer
+
+        model = Model(
+            ndarray=np.array([[1, 2], [3, 4]]),
+            npfloat=np.float64(1.3),
+            npinteger=np.int32(5),
+        )
+        assert make_encodeable(model) == {
+            "ndarray": [[1, 2], [3, 4]],
+            "npfloat": 1.3,
+            "npinteger": 5,
+        }
