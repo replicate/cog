be9f8a4f2dc1ec72d26cbafdf45e09cd8feba64a
Tim Fall <tim@replicate.com>
2024-05-31 12:17:49 -0700

    Quick fix for Apple Silicon M1 support in `run`
    This commit fixes the `run` command for Apple Silicon M1 support. The `run` command now uses the `docker run` command with the `--platform` flag set to `linux/amd64` to ensure that the container runs on the correct architecture.
    This behavior matches the `build` command, which also uses the `--platform` flag to ensure that the image is built for the correct architecture.

--- pkg/cli/run.go
+++ pkg/cli/run.go
@@ -9,6 +9,7 @@ import (
 	"github.com/replicate/cog/pkg/config"
 	"github.com/replicate/cog/pkg/docker"
 	"github.com/replicate/cog/pkg/image"
+	"github.com/replicate/cog/pkg/util"
 	"github.com/replicate/cog/pkg/util/console"
 )
 
@@ -71,6 +72,10 @@ func run(cmd *cobra.Command, args []string) error {
 		Image:   imageName,
 		Volumes: []docker.Volume{{Source: projectDir, Destination: "/src"}},
 		Workdir: "/src",
+	}
+
+	if util.IsAppleSiliconMac(os.GOOS, os.GOARCH) {
+		runOptions.Platform = "linux/amd64"
 	}
 
 	for _, portString := range runPorts {
