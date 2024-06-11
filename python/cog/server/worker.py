8a44056363aa6a62cbbd9d48a261b5626711eaa4
technillogue <technillogue@gmail.com>
2024-06-11 14:19:39 -0400

    log traceback
    Signed-off-by: technillogue <technillogue@gmail.com>

--- python/cog/server/worker.py
+++ python/cog/server/worker.py
@@ -243,7 +243,8 @@ class _ChildWorker(_spawn.Process):  # type: ignore
         except asyncio.CancelledError:
             done.canceled = True
         except Exception as e:
-            traceback.print_exc()
+            tb = traceback.format_exc()
+            self._log(tb)
             done.error = True
             done.error_detail = str(e)
         finally:
@@ -313,7 +314,7 @@ class _ChildWorker(_spawn.Process):  # type: ignore
             original_stream.write(data)
             original_stream.flush()
         # this won't work, this fn gets called from a thread, not the async task
-        self._log(data, stream_name)
+        self._log(data, source=stream_name)
 
 
 def get_loop() -> asyncio.AbstractEventLoop:
