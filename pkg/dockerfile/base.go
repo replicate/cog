349960f7ed95ede047df00d9f394ad2be01e03cc
Andreas Jansson <andreas@replicate.ai>
2024-06-26 14:23:33 +0200

    Support minor torch version (#1769)
    * Bump github.com/docker/docker to v27.0.1+incompatible
    
    Signed-off-by: andreasjansson <andreas@replicate.ai>
    
    * Support minor torch version
    
    * Generate minor Torch version compatibility matrix based on existing patch-version based compatibility matrix, using latest patch version
    * Remove CUDA patch version resolution in Torch path
    
    Signed-off-by: andreasjansson <andreas@replicate.ai>
    
    * Remove duplicate declaration of TestPythonPackagesBothTorchAndTensorflow
    
    ---------
    
    Signed-off-by: andreasjansson <andreas@replicate.ai>
    Co-authored-by: Mattt Zmuda <mattt@replicate.com>

--- pkg/dockerfile/base.go
+++ pkg/dockerfile/base.go
@@ -3,7 +3,6 @@ package dockerfile
 import (
 	"encoding/json"
 	"fmt"
-	"sort"
 	"strings"
 
 	"github.com/replicate/cog/pkg/config"
@@ -103,13 +102,6 @@ func (b BaseImageConfiguration) MarshalJSON() ([]byte, error) {
 // with patch versions stripped out. Each version is greater or equal to
 // MinimumCUDAVersion/MinimumPythonVersion/MinimumTorchVersion.
 func BaseImageConfigurations() []BaseImageConfiguration {
-	compatMatrixSortedByTorchDesc := make([]config.TorchCompatibility, len(config.TorchCompatibilityMatrix))
-	copy(compatMatrixSortedByTorchDesc, config.TorchCompatibilityMatrix)
-	sort.Slice(compatMatrixSortedByTorchDesc, func(i, j int) bool {
-		return version.Greater(compatMatrixSortedByTorchDesc[j].Torch, compatMatrixSortedByTorchDesc[i].Torch)
-	})
-
-	configsSet := make(map[BaseImageConfiguration]bool)
 	configs := []BaseImageConfiguration{}
 
 	// Assuming that the Torch versions cover all Python and CUDA versions to avoid
@@ -118,7 +110,7 @@ func BaseImageConfigurations() []BaseImageConfiguration {
 	cudaVersionsSet := make(map[string]bool)
 
 	// Torch configs
-	for _, compat := range compatMatrixSortedByTorchDesc {
+	for _, compat := range config.TorchMinorCompatibilityMatrix {
 		for _, python := range compat.Pythons {
 
 			// Only support fast cold boots for Torch with CUDA.
@@ -137,10 +129,8 @@ func BaseImageConfigurations() []BaseImageConfiguration {
 
 			if (version.GreaterOrEqual(cuda, MinimumCUDAVersion)) &&
 				version.GreaterOrEqual(python, MinimumPythonVersion) &&
-				version.GreaterOrEqual(compat.Torch, MinimumTorchVersion) &&
-				!configsSet[conf] {
+				version.GreaterOrEqual(compat.Torch, MinimumTorchVersion) {
 				configs = append(configs, conf)
-				configsSet[conf] = true
 				pythonVersionsSet[python] = true
 				cudaVersionsSet[cuda] = true
 			}
