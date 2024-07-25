b9926bea90ddebdc0091496fe9dc725faf7d74c8
Mattt Zmuda <mattt@replicate.com>
2024-07-25 04:35:24 -0700

    Fix Pyright warning: Condition will always evaluate to False since the types type[BaseModel] and None have no overlap (reportUnnecessaryComparison)

--- python/cog/predictor.py
+++ python/cog/predictor.py
@@ -392,7 +392,7 @@ def get_output_type(predictor: BasePredictor) -> Type[BaseModel]:
     input_types = get_type_hints(predict)
 
     OutputType = input_types.pop("return", None)
-    if OutputType is None:
+    if not OutputType:
         raise TypeError(
             """You must set an output type. If your model can return multiple output types, you can explicitly set `Any` as the output type.
 
