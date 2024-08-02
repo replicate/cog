0976a05c2e0f7fcecd05fabe7d4c9d6a31b6b11e
technillogue <technillogue@gmail.com>
2024-08-02 15:09:16 -0400

    move runner.terminate into runner.shutdown after waiting for predictions to complete (#1843)
    * move runner.terminate into shutdown, make it async, and document the behavior of Server.stop, should_exit, force_exit, and app shutdown handler,
    * remove BaseException handlers or re-raise
    * fix tests
    
    ---------
    
    Signed-off-by: technillogue <technillogue@gmail.com>

--- python/cog/server/runner.py
+++ python/cog/server/runner.py
@@ -198,10 +198,11 @@ class PredictionRunner:
             except Exception:
                 logs.append(traceback.format_exc())
                 status = schema.Status.FAILED
-            except BaseException:
-                self.log.info("caught BaseException during setup, did something go wrong?")
+            except asyncio.CancelledError:
+                self.log.info("caught CancelledError during setup")
                 logs.append(traceback.format_exc())
                 status = schema.Status.FAILED
+                # unclear if we should re-raise this
 
             # fixme: handle BaseException is mux.read times out and gets cancelled
 
@@ -237,9 +238,12 @@ class PredictionRunner:
                 if self._shutdown_event is not None:
                     self._shutdown_event.set()
             except BaseException:
-                self.log.error("caught base exception while running setup", exc_info=True)
+                self.log.error(
+                    "caught base exception while running setup", exc_info=True
+                )
                 if self._shutdown_event is not None:
                     self._shutdown_event.set()
+                raise
 
         result = asyncio.create_task(inner())
         result.add_done_callback(handle_error)
@@ -340,7 +344,7 @@ class PredictionRunner:
                 await event_handler.failed(error=str(e))
                 self.log.warn("failed to download url path from input", exc_info=True)
                 return event_handler.response
-            except Exception as e: # should this be BaseException?
+            except Exception as e:  # should this be BaseException?
                 tb = traceback.format_exc()
                 await event_handler.append_logs(tb)
                 await event_handler.failed(error=str(e))
@@ -371,7 +375,8 @@ class PredictionRunner:
 
         return (response, result)
 
-    def shutdown(self) -> None:
+    async def shutdown(self) -> None:
+        # this is called by the app's shutdown handler. server won't exit until this is done
         self.log.info("runner.shutdown called")
         if self._state == WorkerState.DEFUNCT:
             return
@@ -382,22 +387,33 @@ class PredictionRunner:
             self.log.info("child is alive during shutdown, sending Shutdown event")
             self._events.send(Shutdown())
 
-    def terminate(self) -> None:
-        self.log.info("runner.terminate is called")
-        for _, task in self._predictions.values():
-            task.cancel()
         if self._state == WorkerState.DEFUNCT:
+            self.log.info("worker state is already defunct, no need to terminate")
             return
 
-        self._terminating.set()
+        prediction_tasks = [task for _, task in self._predictions.values()]
+        try:
+            if prediction_tasks:
+                await asyncio.wait(prediction_tasks, timeout=9)
+        # should we do this?
+        except TimeoutError:
+            self.log.warn("runner timeout while waiting for predictions to complete")
+
         self._state = WorkerState.DEFUNCT
+        # in case we timed out, cancel everything
+        for task in prediction_tasks:
+            task.cancel()
+
+        # tell _read_events and Mux to exit
+        self._terminating.set()
 
         if self._child.is_alive():
             self._child.terminate()
             self.log.info("joining child worker")
             self._child.join()
+        # close the pipe
         self._events.close()
-
+        # stop reading events from the pipe
         if self._read_events_task:
             self._read_events_task.cancel()
 
