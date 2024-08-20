d8df716c91300741913b8c4ce3d274bdf3017290
Will Sackfield <sackfield@replicate.com>
2024-08-20 17:38:02 -0400

    Set useCogBaseImage to false in base-image
    * We never want to useCogBaseImage when generating
    base images.

--- pkg/dockerfile/base_test.go
+++ pkg/dockerfile/base_test.go
@@ -1,6 +1,7 @@
 package dockerfile
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -27,4 +28,16 @@ func TestBaseImageName(t *testing.T) {
 		actual := BaseImageName(tt.cuda, tt.python, tt.torch)
 		require.Equal(t, tt.expected, actual)
 	}
+}
+
+func TestGenerateDockerfile(t *testing.T) {
+	generator, err := NewBaseImageGenerator(
+		"12.1",
+		"3.8",
+		"2.1.0",
+	)
+	require.NoError(t, err)
+	dockerfile, err := generator.GenerateDockerfile()
+	require.NoError(t, err)
+	require.True(t, strings.Contains(dockerfile, "FROM nvidia/cuda:12.1.1-cudnn8-devel-ubuntu22.04"))
 }
