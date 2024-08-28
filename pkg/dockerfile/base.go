1d1daf663491d4b4dc476484d05d213497e1d597
Will Sackfield <sackfield@replicate.com>
2024-08-28 14:13:59 -0700

    Fix generating matrix with all CPU torches (#1913)
    * We need to check the python and torch minimum
    versions before we add that config to the matrix

--- pkg/dockerfile/base.go
+++ pkg/dockerfile/base.go
@@ -112,29 +112,28 @@ func BaseImageConfigurations() []BaseImageConfiguration {
 	// Torch configs
 	for _, compat := range config.TorchCompatibilityMatrix {
 		for _, python := range compat.Pythons {
+			if !version.GreaterOrEqual(python, MinimumPythonVersion) || !version.GreaterOrEqual(compat.Torch, MinimumTorchVersion) {
+				continue
+			}
 
 			if compat.CUDA == nil {
 				configs = append(configs, BaseImageConfiguration{
 					PythonVersion: python,
 					TorchVersion:  compat.Torch,
 				})
-				continue
-			}
-
-			cuda := *compat.CUDA
-			torch := compat.Torch
-			conf := BaseImageConfiguration{
-				CUDAVersion:   cuda,
-				PythonVersion: python,
-				TorchVersion:  torch,
-			}
-
-			if (version.GreaterOrEqual(cuda, MinimumCUDAVersion)) &&
-				version.GreaterOrEqual(python, MinimumPythonVersion) &&
-				version.GreaterOrEqual(compat.Torch, MinimumTorchVersion) {
-				configs = append(configs, conf)
-				pythonVersionsSet[python] = true
-				cudaVersionsSet[cuda] = true
+			} else {
+				cuda := *compat.CUDA
+				torch := compat.Torch
+				conf := BaseImageConfiguration{
+					CUDAVersion:   cuda,
+					PythonVersion: python,
+					TorchVersion:  torch,
+				}
+				if version.GreaterOrEqual(cuda, MinimumCUDAVersion) {
+					configs = append(configs, conf)
+					pythonVersionsSet[python] = true
+					cudaVersionsSet[cuda] = true
+				}
 			}
 		}
 	}
