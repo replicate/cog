b1a616a0ebec30e02ec18123a4cf5d7ae869a692
Will Sackfield <sackfield@replicate.com>
2024-08-28 13:12:35 -0700

    Add Cython CFLAGS for package optimizations (#1904)
    * Add Cython CLFAGS for package optimizations
    
    * These flags should make the code faster and
    smaller
    * This will affect cpython generated shared
    libraries
    
    * Use ENV to set/unset CFLAGS
    
    * Wrap CFLAGS around pip installs
    
    * Combine append lines
    
    * Fix gocritic error of multiple append lines.
    
    * Remove —ffast-math for CFLAGS
    
    * This has a problem of stomping on float
    subnormals
    * This can adversely affect ML workflows

--- pkg/dockerfile/generator.go
+++ pkg/dockerfile/generator.go
@@ -42,6 +42,7 @@ coverage.xml
 .pytest_cache
 .hypothesis
 `
+const CFlags = "ENV CFLAGS=\"-O3 -march=native -funroll-loops -fno-strict-aliasing -flto -mtune=native -S\""
 
 type Generator struct {
 	Config *config.Config
@@ -363,7 +364,8 @@ RUN --mount=type=cache,target=/var/cache/apt,sharing=locked apt-get update -qq &
 	git \
 	ca-certificates \
 	&& rm -rf /var/lib/apt/lists/*
-` + fmt.Sprintf(`RUN curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash && \
+` + fmt.Sprintf(`
+RUN curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash && \
 	git clone https://github.com/momo-lab/pyenv-install-latest.git "$(pyenv root)"/plugins/pyenv-install-latest && \
 	export PYTHON_CONFIGURE_OPTS='--enable-optimizations --with-lto' && \
 	export PYTHON_CFLAGS='-march=native -mtune=native -O3' && \
@@ -393,7 +395,7 @@ func (g *Generator) installCog() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	lines = append(lines, fmt.Sprintf("RUN --mount=type=cache,target=/root/.cache/pip pip install -t /dep %s", containerPath))
+	lines = append(lines, CFlags, fmt.Sprintf("RUN --mount=type=cache,target=/root/.cache/pip pip install -t /dep %s", containerPath), "ENV CFLAGS=")
 	return strings.Join(lines, "\n"), nil
 }
 
@@ -426,7 +428,9 @@ func (g *Generator) pipInstalls() (string, error) {
 
 	return strings.Join([]string{
 		copyLine[0],
+		CFlags,
 		"RUN pip install -r " + containerPath,
+		"ENV CFLAGS=",
 	}, "\n"), nil
 }
 
@@ -466,7 +470,9 @@ func (g *Generator) pipInstallStage() (string, error) {
 		fromLine,
 		installCog,
 		copyLine[0],
+		CFlags,
 		"RUN --mount=type=cache,target=/root/.cache/pip pip install -t /dep -r " + containerPath,
+		"ENV CFLAGS=",
 	}
 	return strings.Join(lines, "\n"), nil
 }
