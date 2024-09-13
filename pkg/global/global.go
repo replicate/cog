dbfa22f62ee6262927965ec379e20490d36d57bb
Will Sackfield <sackfield@replicate.com>
2024-09-13 10:45:42 -0700

    Add timeout flag to predict (#1879)
    * Add timeout flag to predict
    
    * Allow the user of cog to specify the timeout
    when performing predictions.
    * This allows some models doing a lot in their
    setup to not automatically time out and leaves
    that parameter up to the user rather than a
    hardcoded global.
    
    * Use setup-timeout argument instead of timeout
    
    * Specify that this timeout is specifically for
    setups

--- pkg/global/global.go
+++ pkg/global/global.go
@@ -1,16 +1,11 @@
 package global
 
-import (
-	"time"
-)
-
 var (
 	Version               = "dev"
 	Commit                = ""
 	BuildTime             = "none"
 	Debug                 = false
 	ProfilingEnabled      = false
-	StartupTimeout        = 5 * time.Minute
 	ConfigFilename        = "cog.yaml"
 	ReplicateRegistryHost = "r8.im"
 	ReplicateWebsiteHost  = "replicate.com"
