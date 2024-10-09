0031765b1c0b1e756c918948b306cd742977457d
Will Sackfield <sackfield@replicate.com>
2024-10-09 16:35:19 -0400

    Remove predictor import

--- python/cog/server/runner.py
+++ python/cog/server/runner.py
@@ -16,7 +16,6 @@ from .. import schema
 from ..base_input import BaseInput
 from ..files import put_file_to_signed_endpoint
 from ..json import upload_files
-from ..predictor import BaseInput
 from .errors import FileUploadError, RunnerBusyError, UnknownPredictionError
 from .eventtypes import Done, Log, PredictionOutput, PredictionOutputType
 from .telemetry import current_trace_context
