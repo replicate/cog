8d6c4ce17b4d01da6b25b99f2fc6d7ed2f9881fc
Will Sackfield <sackfield@replicate.com>
2024-10-16 14:01:51 -0400

    Fix imports after merge

--- python/cog/predictor.py
+++ python/cog/predictor.py
@@ -6,7 +6,6 @@ import io
 import os.path
 import sys
 import types
-import uuid
 from collections.abc import Iterable, Iterator
 from pathlib import Path
 from typing import (
@@ -38,8 +37,6 @@ from typing_extensions import Annotated
 
 from .base_input import BaseInput
 from .base_predictor import BasePredictor
-from .code_xforms import load_module_from_string, strip_model_source_code
-from .errors import ConfigDoesNotExist, PredictorNotSet
 from .types import (
     PYDANTIC_V2,
     Input,
