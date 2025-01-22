a53a46384388a6d011c5f4e7d64c4d4f17a9cbe4
andreasjansson <andreas@replicate.ai>
2025-01-22 18:55:47 +0100

    Add default_example to included function, and catch failures

--- python/cog/include.py
+++ python/cog/include.py
@@ -1,6 +1,11 @@
+from dataclasses import dataclass
 import inspect
 from typing import Callable, Any
 import replicate
+from replicate.model import Model
+from replicate.version import Version
+from replicate.prediction import Prediction
+from replicate.exceptions import ModelError
 
 
 def _find_api_token() -> str:
@@ -15,23 +20,48 @@ def _find_api_token() -> str:
     raise ValueError("No run token found in call stack")
 
 
-def include(model_path: str) -> Callable[..., Any]:
-    def run(**inputs: dict[str, Any]) -> Any:
-        client = replicate.Client(api_token=_find_api_token())
-        model_owner, model_name = model_path.split("/")
-        model_name, model_version = (
-            model_name.split(":") if ":" in model_name else (model_name, None)
-        )
+@dataclass
+class Run:
+    function_ref: str
 
+    def _client(self) -> replicate.Client:
+        return replicate.Client(api_token=_find_api_token())
+
+    def _split_function_ref(self) -> tuple[str, str, str | None]:
+        owner, name = self.function_ref.split("/")
+        name, version = name.split(":") if ":" in name else (name, None)
+        return owner, name, version
+
+    def __call__(self, **inputs: dict[str, Any]):
+        version = self._version()
+        prediction = self._client().predictions.create(version=version, input=inputs)
+        print(f"Running prediction https://replicate.com/p/{prediction.id}")
+
+        prediction.wait()
+
+        if prediction.status == "failed":
+            raise ModelError(prediction)
+
+        return prediction.output
+
+    def _model(self) -> Model:
+        client = self._client()
+        model_owner, model_name, _ = self._split_function_ref()
+        return client.models.get(f"{model_owner}/{model_name}")
+
+    def _version(self) -> Version:
+        client = self._client()
+        model_owner, model_name, model_version = self._split_function_ref()
         model = client.models.get(f"{model_owner}/{model_name}")
         version = (
             model.versions.get(model_version) if model_version else model.latest_version
         )
+        return version
 
-        prediction = client.predictions.create(version=version, input=inputs)
-        print(f"Running prediction {prediction.id}")
+    @property
+    def default_example(self) -> Prediction | None:
+        return self._model().default_example
 
-        prediction.wait()
-        return prediction.output
 
-    return run
+def include(model: str) -> Callable[..., Any]:
+    return Run(model)
