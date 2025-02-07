a2737aaa7da92add2b5c785380020bd00c278bc6
Will Sackfield <sackfield@replicate.com>
2025-02-07 16:36:11 -0500

    Add web/monobeam client tests

---
+++ pkg/monobeam/client_test.go
@@ -0,0 +1,54 @@
+package monobeam
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/vbauerster/mpb/v8"
+
+	"github.com/replicate/cog/pkg/env"
+	"github.com/replicate/cog/pkg/weights"
+)
+
+func TestUploadFile(t *testing.T) {
+	// Setup mock http server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusConflict)
+	}))
+	defer server.Close()
+	url, err := url.Parse(server.URL)
+	require.NoError(t, err)
+	t.Setenv(env.SchemeEnvVarName, url.Scheme)
+	t.Setenv(MonobeamHostEnvVarName, url.Host)
+
+	dir := t.TempDir()
+
+	// Create mock weight
+	data := make([]byte, 1024)
+	for i := 0; i < len(data); i++ {
+		data[i] = byte(i % 256)
+	}
+	weightPath := filepath.Join(dir, "test_weight")
+	file, err := os.Create(weightPath)
+	require.NoError(t, err)
+	defer file.Close()
+	for i := 0; i <= ((weights.WEIGHT_FILE_SIZE_INCLUSION+1)/1024)+1; i++ {
+		_, err := file.Write(data)
+		require.NoError(t, err)
+	}
+
+	client := NewClient(http.DefaultClient)
+	ctx := context.Background()
+	p := mpb.New(
+		mpb.WithRefreshRate(180 * time.Millisecond),
+	)
+	err = client.UploadFile(ctx, "weights", "111", weightPath, p, "weights - "+weightPath)
+	require.NoError(t, err)
+}
---
+++ pkg/web/client_test.go
@@ -0,0 +1,45 @@
+package web
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/replicate/cog/pkg/docker/dockertest"
+	"github.com/replicate/cog/pkg/env"
+)
+
+func TestPostNewVersion(t *testing.T) {
+	// Setup mock http server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusCreated)
+	}))
+	defer server.Close()
+	url, err := url.Parse(server.URL)
+	require.NoError(t, err)
+	t.Setenv(env.SchemeEnvVarName, url.Scheme)
+	t.Setenv(WebHostEnvVarName, url.Host)
+
+	dir := t.TempDir()
+
+	// Create mock predict
+	predictPyPath := filepath.Join(dir, "predict.py")
+	handle, err := os.Create(predictPyPath)
+	require.NoError(t, err)
+	handle.WriteString("import cog")
+	dockertest.MockCogConfig = "{\"build\":{\"python_version\":\"3.12\",\"python_packages\":[\"torch==2.5.0\",\"beautifulsoup4==4.12.3\"],\"system_packages\":[\"git\"]},\"image\":\"test\",\"predict\":\"" + predictPyPath + ":Predictor\"}"
+
+	// Setup mock command
+	command := dockertest.NewMockCommand()
+
+	client := NewClient(command, http.DefaultClient)
+	ctx := context.Background()
+	err = client.PostNewVersion(ctx, "test", []File{}, []File{})
+	require.NoError(t, err)
+}
