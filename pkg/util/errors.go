8ccbddf812cbaa9d00368b013f2bb1819ca2473f
Nikhil Sinha <nikhil@replicate.com>
2025-02-11 11:57:53 -0800

    Add comment explaining WrapError
    Signed-off-by: Nikhil Sinha <nikhil@replicate.com>

--- pkg/util/errors.go
+++ pkg/util/errors.go
@@ -2,6 +2,8 @@ package util
 
 import "fmt"
 
+// WrapError is just a shortcut for using fmt.Errorf
+// to wrap an error with a message
 func WrapError(err error, message string) error {
 	if err == nil {
 		return nil
