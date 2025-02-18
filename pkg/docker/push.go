224a33fab63c293f0dec5fd2269f9b4cc34bed49
Nikhil Sinha <nikhil@replicate.com>
2025-02-17 17:29:18 -0800

    Move random hash to function
    Signed-off-by: Nikhil Sinha <nikhil@replicate.com>

--- pkg/docker/push.go
+++ pkg/docker/push.go
@@ -26,13 +26,7 @@ func Push(image string, fast bool, projectDir string, command command.Command, b
 	// this is just a loss of push timing information
 	imageID := ""
 	if fast {
-		// Generate 256 bit random hash (4x64 bits) to use as an upload ID
-		for i := 0; i < 4; i++ {
-			// Ignoring the linter warning about math/rand/v2 not being cryptographically secure
-			// because this just needs to be a "unique enough" ID for a cache between when the
-			// push starts and ends, which should only be ~a week max.
-			imageID = fmt.Sprintf("%s%x", imageID, rand.Int64()) //nolint:gosec
-		}
+		imageID = buildRandomHash256()
 	} else {
 		imageMeta, err := command.Inspect(image)
 		if err != nil {
@@ -55,4 +49,16 @@ func Push(image string, fast bool, projectDir string, command command.Command, b
 		return FastPush(ctx, image, projectDir, command, webClient, monobeamClient, imageID)
 	}
 	return StandardPush(image, command)
+}
+
+func buildRandomHash256() string {
+	out := ""
+	// Generate 256 bit random hash (4x64 bits) to use as an upload ID
+	for i := 0; i < 4; i++ {
+		// Ignoring the linter warning about math/rand/v2 not being cryptographically secure
+		// because this just needs to be a "unique enough" ID for a cache between when the
+		// push starts and ends, which should only be ~a week max.
+		out = fmt.Sprintf("%s%x", out, rand.Int64()) //nolint:gosec
+	}
+	return out
 }
