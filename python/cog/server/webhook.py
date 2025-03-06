4a3201bcf7e1b2974738abfd57e379939131e1ef
Will Sackfield <sackfield@replicate.com>
2025-03-06 10:23:54 -0500

    Encode the payload to json after dict serialisation
    * upload_files needs to know if the payload has a
    Path object

--- python/cog/server/webhook.py
+++ python/cog/server/webhook.py
@@ -49,17 +49,17 @@ def webhook_caller(webhook: str) -> Callable[[Any], None]:
     def caller(response: PredictionResponse) -> None:
         if throttler.should_send_response(response):
             if PYDANTIC_V2:
-                dict_response = jsonable_encoder(
-                    response.model_dump(exclude_unset=True)
-                )
+                response_object = response.model_dump(exclude_unset=True)
             else:
-                dict_response = jsonable_encoder(response.dict(exclude_unset=True))
+                response_object = response.dict(exclude_unset=True)
 
-            if "output" in dict_response:
-                dict_response["output"] = upload_files(
-                    dict_response["output"],
+            if "output" in response_object:
+                response_object["output"] = upload_files(
+                    response_object["output"],
                     upload_file=upload_file,  # type: ignore
                 )
+
+            dict_response = jsonable_encoder(response_object)
 
             if Status.is_terminal(response.status):
                 # For terminal updates, retry persistently
