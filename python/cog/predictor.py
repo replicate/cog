8b0f43e4f0987af1af40642e5c0d5a101b7e6516
Will Sackfield <sackfield@replicate.com>
2025-03-19 16:09:05 -0400

    Remove Optional import from typing_extensions

--- python/cog/predictor.py
+++ python/cog/predictor.py
@@ -29,7 +29,7 @@ from pydantic import BaseModel, Field, create_model
 from pydantic.fields import FieldInfo
 
 # Added in Python 3.9. Can be from typing if we drop support for <3.9
-from typing_extensions import Annotated, Optional
+from typing_extensions import Annotated
 
 from .base_input import BaseInput
 from .base_predictor import BasePredictor
