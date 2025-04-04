beba0424aba813033c1d03eeb3bae9f3d17a5be7
Dan Buch <dan@meatballhat.com>
2025-04-04 11:39:01 -0400

    Add empty namespace package for `cog.ext` (#2246)
    to see if it's an off by one issue that's preventing extension packages
    from directly depending on `cog`.

---
+++ python/cog/ext/__init__.py
@@ -0,0 +1,3 @@
+# This `cog/ext` directory is a [namespace
+# directory](https://packaging.python.org/en/latest/guides/packaging-namespace-packages/)
+# that intentionally does not contain any production code.
