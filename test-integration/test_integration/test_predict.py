e1712761414158e1681275ff7f2a71958a522636
Will Sackfield <sackfield@replicate.com>
2025-04-16 17:40:17 -0400

    Add zsh integration test

--- test-integration/test_integration/test_predict.py
+++ test-integration/test_integration/test_predict.py
@@ -432,3 +432,25 @@ def test_predict_optional_project(tmpdir_factory):
     # stdout should be clean without any log messages so it can be piped to other commands
     assert result.returncode == 0
     assert result.stdout == "hello No One\n"
+
+
+def test_predict_zsh_package(docker_image):
+    project_dir = Path(__file__).parent / "fixtures/zsh-package"
+    build_process = subprocess.run(
+        ["cog", "build", "-t", docker_image],
+        cwd=project_dir,
+        capture_output=True,
+    )
+    assert build_process.returncode == 0
+
+    result = subprocess.run(
+        ["cog", "predict", "--debug", docker_image],
+        cwd=project_dir,
+        check=True,
+        capture_output=True,
+        text=True,
+        timeout=DEFAULT_TIMEOUT,
+    )
+    # stdout should be clean without any log messages so it can be piped to other commands
+    assert result.returncode == 0
+    assert result.stdout == "hello No One\n"
