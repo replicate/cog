aa1d36ab16273f40cea01d815aa911d153be8a16
Will Sackfield <sackfield@replicate.com>
2025-05-07 11:07:46 -0400

    Add unit test for gpu migration

--- pkg/migrate/migrator_v1_v1fast_test.go
+++ pkg/migrate/migrator_v1_v1fast_test.go
@@ -97,3 +97,67 @@ class Predictor(BasePredictor):
 	err = file.Close()
 	require.NoError(t, err)
 }
+
+func TestMigrateGPU(t *testing.T) {
+	// Set our new working directory to a temp directory
+	originalDir, err := os.Getwd()
+	require.NoError(t, err)
+	defer func() {
+		err := os.Chdir(originalDir)
+		require.NoError(t, err)
+	}()
+	dir := t.TempDir()
+	err = os.Chdir(dir)
+	require.NoError(t, err)
+
+	// Write our test configs/code
+	configFilepath := filepath.Join(dir, "cog.yaml")
+	file, err := os.Create(configFilepath)
+	require.NoError(t, err)
+	_, err = file.WriteString(`build:
+  python_version: "3.11"
+  fast: true
+  python_packages:
+    - "pillow"
+  run:
+    - command: curl -o /usr/local/bin/pget -L \"https://github.com/replicate/pget/releases/latest/download/pget_$(uname -s)_$(uname -m)\" && chmod +x /usr/local/bin/pget
+  gpu: true
+predict: "predict.py:Predictor"
+`)
+	require.NoError(t, err)
+	err = file.Close()
+	require.NoError(t, err)
+	pythonFilepath := filepath.Join(dir, "predict.py")
+	file, err = os.Create(pythonFilepath)
+	require.NoError(t, err)
+	_, err = file.WriteString(`from cog import BasePredictor, Input
+
+
+class Predictor(BasePredictor):
+    def predict(self, s: str = Input(description="My Input Description", default=None)) -> str:
+        return "hello " + s
+`)
+	require.NoError(t, err)
+	err = file.Close()
+	require.NoError(t, err)
+
+	// Perform the migration
+	migrator := NewMigratorV1ToV1Fast(false)
+	err = migrator.Migrate(t.Context(), "cog.yaml")
+	require.NoError(t, err)
+
+	// Check config output
+	file, err = os.Open(configFilepath)
+	require.NoError(t, err)
+	content, err := io.ReadAll(file)
+	require.NoError(t, err)
+	require.Equal(t, `build:
+  python_version: "3.11"
+  python_requirements: requirements.txt
+  fast: true
+  gpu: true
+predict: predict.py:Predictor
+`, string(content))
+	err = file.Close()
+	require.NoError(t, err)
+}
