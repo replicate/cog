4f4b4e1da51ae50afb3c5c4826653ee7ac507663
Will Sackfield <sackfield@replicate.com>
2025-05-09 16:56:02 -0400

    Add support for lists of dicts in cog predict (#2317)
    * Add support for lists of dicts in cog predict
    
    * Allow cog predict to handle JSON lists
    
    * Use reflection to figure out type
    
    * Handle when JSON can’t deserialise in an array

---
+++ test-integration/test_integration/fixtures/complex-types-list-project/predict.py
@@ -0,0 +1,11 @@
+from anthropic.types import MessageParam
+from cog import BasePredictor, Input
+from cog.coder import json_coder  # noqa: F401
+
+
+class Predictor(BasePredictor):
+    def predict(
+        self,
+        messages: list[MessageParam] = Input(description="Messages API."),
+    ) -> str:
+        return "Content: " + "-".join([x["content"] for x in messages])
