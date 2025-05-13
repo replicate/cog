9578577af00495ef8147e6e16d0370eca0b13e3b
Michael Dwan <m@dwan.io>
2025-05-13 12:27:14 -0600

    restrict test registry port so docker insecure works

--- pkg/registry_testhelpers/registry_container.go
+++ pkg/registry_testhelpers/registry_container.go
@@ -5,13 +5,19 @@ import (
 	"path"
 	"path/filepath"
 	"runtime"
+	"strconv"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/go-connections/nat"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/registry"
 	"github.com/testcontainers/testcontainers-go/wait"
+
+	"github.com/replicate/cog/pkg/util"
 )
 
 // StartTestRegistry starts a test registry container on a random local port populated
@@ -37,6 +43,16 @@ func StartTestRegistry(t *testing.T) *RegistryContainer {
 			wait.ForHTTP("/v2/").WithPort("5000/tcp").
 				WithStartupTimeout(10*time.Second),
 		),
+		testcontainers.WithHostConfigModifier(func(hostConfig *container.HostConfig) {
+			// docker only considers localhost:1 through localhost:9999 as insecure. testcontainers
+			// picks higher ports by default, so we need to pick one ourselves to allow insecure access
+			// without modifying the daemon config.
+			port, err := util.PickFreePort(1024, 9999)
+			require.NoError(t, err, "Failed to pick free port")
+			hostConfig.PortBindings = map[nat.Port][]nat.PortBinding{
+				nat.Port("5000/tcp"): {{HostIP: "0.0.0.0", HostPort: strconv.Itoa(port)}},
+			}
+		}),
 	)
 	defer testcontainers.CleanupContainer(t, registryContainer)
 	require.NoError(t, err, "Failed to start registry container")
@@ -56,5 +72,6 @@ func (c *RegistryContainer) ImageRefForTest(t *testing.T, label string) string {
 	if label == "" {
 		label = fmt.Sprintf("test-%d", time.Now().Unix())
 	}
-	return c.ImageRef(fmt.Sprintf("%s:%s", t.Name(), label))
+	repo := strings.ToLower(t.Name())
+	return c.ImageRef(fmt.Sprintf("%s:%s", repo, label))
 }
