703ce4031b0f4f20a7cc3a321f58e56d62528e12
Michael Dwan <m@dwan.io>
2025-05-15 14:35:12 -0600

    missed some stubs from the other branch

--- pkg/docker/docker.go
+++ pkg/docker/docker.go
@@ -13,7 +13,8 @@ func NewClient(ctx context.Context, opts ...Option) (command.Command, error) {
 	enabled, _ := strconv.ParseBool(os.Getenv("COG_DOCKER_SDK_CLIENT"))
 	if enabled {
 		console.Debugf("Docker client: sdk")
-		return NewAPIClient(ctx, opts...)
+		panic("not implemented yet")
+		// return NewAPIClient(ctx, opts...)
 	}
 
 	console.Debugf("Docker client: cli")
