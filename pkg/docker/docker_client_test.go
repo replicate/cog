74287d126c0bd0a4933e05c59478b05c965d5201
Michael Dwan <m@dwan.io>
2025-05-16 20:42:15 -0600

    set env var so subtests pickup the right client

--- pkg/docker/docker_client_test.go
+++ pkg/docker/docker_client_test.go
@@ -24,6 +24,8 @@ func TestDockerClient(t *testing.T) {
 		t.Skip("skipping docker client tests in short mode")
 	}
 
+	t.Setenv("COG_DOCKER_SDK_CLIENT", "0")
+
 	client := NewDockerCommand()
 	runDockerClientTests(t, client)
 }
@@ -32,6 +34,8 @@ func TestDockerAPIClient(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping docker client tests in short mode")
 	}
+
+	t.Setenv("COG_DOCKER_SDK_CLIENT", "1")
 
 	apiClient, err := NewAPIClient(t.Context())
 	require.NoError(t, err, "Failed to create docker api client")
