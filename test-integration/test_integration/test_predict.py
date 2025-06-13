95cf21a681bb901fc384d8b248ae2edf50fa9d8d
Will Sackfield <sackfield@replicate.com>
2025-06-13 12:17:39 -0400

    Add test_predict_json_input integration test

--- test-integration/test_integration/test_predict.py
+++ test-integration/test_integration/test_predict.py
@@ -618,3 +618,24 @@ def test_predict_tensorflow_project(docker_image, cog_binary):
     )
     assert result.returncode == 0
     assert result.stdout == "2.10.0\n"
+
+
+def test_predict_json_input(docker_image, cog_binary):
+    project_dir = Path(__file__).parent / "fixtures/string-project"
+
+    result = subprocess.run(
+        [
+            cog_binary,
+            "predict",
+            "--debug",
+            "--json",
+            '{"s": "sackfield"}',
+        ],
+        cwd=project_dir,
+        check=True,
+        capture_output=True,
+        text=True,
+        timeout=120.0,
+    )
+    assert result.returncode == 0
+    assert result.stdout == "2.10.0\n"
