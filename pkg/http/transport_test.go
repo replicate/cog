c95dc88b8430ea3d568dddc8855afc6ecaee6f17
Will Sackfield <sackfield@replicate.com>
2025-06-13 12:38:51 -0400

    Send explicit error if no token supplied (#2406)
    * Send explicit error if no token supplied
    
    * Send an explicit error that no token has been
    supplied for an authenticated endpoint
    
    * Update pkg/http/transport.go
    
    Co-authored-by: Nikhil Sinha <53827911+NikhilSinha1@users.noreply.github.com>
    Signed-off-by: Will Sackfield <will.sackfield@gmail.com>
    
    ---------
    
    Signed-off-by: Will Sackfield <will.sackfield@gmail.com>
    Co-authored-by: Nikhil Sinha <53827911+NikhilSinha1@users.noreply.github.com>

--- pkg/http/transport_test.go
+++ pkg/http/transport_test.go
@@ -3,6 +3,7 @@ package http
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -50,4 +51,25 @@ func TestTransportOnlyAddsHeaderIfMissing(t *testing.T) {
 	resp, err := transport.RoundTrip(req)
 	require.NoError(t, err)
 	require.Equal(t, resp.Request.Header.Get(testHeader), expectedValue)
+}
+
+func TestTransportSendsErrorWithMissingToken(t *testing.T) {
+	// Setup mock http server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+	u, err := url.Parse(server.URL)
+	require.NoError(t, err)
+
+	transport := Transport{
+		authentication: map[string]string{
+			u.Host: BearerHeaderPrefix + "",
+		},
+	}
+	req, err := http.NewRequest("GET", server.URL, nil)
+	require.NoError(t, err)
+	resp, err := transport.RoundTrip(req)
+	require.Error(t, err)
+	require.Nil(t, resp)
 }
