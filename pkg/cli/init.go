dc84c094190c865324446c012fddafe515933264
Mark Phelps <209477+markphelps@users.noreply.github.com>
2025-06-30 14:01:19 -0400

    chore: just skip by default
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- pkg/cli/init.go
+++ pkg/cli/init.go
@@ -28,11 +28,6 @@ var actionsWorkflowContent []byte
 //go:embed init-templates/requirements.txt
 var requirementsTxtContent []byte
 
-var (
-	skipExisting      bool
-	overwriteExisting bool
-)
-
 func newInitCommand() *cobra.Command {
 	var cmd = &cobra.Command{
 		Use:        "init",
@@ -42,18 +37,11 @@ func newInitCommand() *cobra.Command {
 		Args:       cobra.MaximumNArgs(0),
 	}
 
-	cmd.Flags().BoolVarP(&skipExisting, "skip-existing", "s", false, "Skip all existing files without prompting")
-	cmd.Flags().BoolVarP(&overwriteExisting, "overwrite-existing", "o", false, "Overwrite all existing files without prompting")
-
 	return cmd
 }
 
 func initCommand(cmd *cobra.Command, args []string) error {
 	console.Infof("\nSetting up the current directory for use with Cog...\n")
-
-	if skipExisting && overwriteExisting {
-		return fmt.Errorf("Cannot specify both --skip-existing and --overwrite-existing")
-	}
 
 	cwd, err := os.Getwd()
 	if err != nil {
@@ -68,8 +56,6 @@ func initCommand(cmd *cobra.Command, args []string) error {
 		"requirements.txt":            requirementsTxtContent,
 	}
 
-	globalSkip := false
-
 	for filename, content := range fileContentMap {
 		filePath := path.Join(cwd, filename)
 		fileExists, err := files.Exists(filePath)
@@ -77,58 +63,22 @@ func initCommand(cmd *cobra.Command, args []string) error {
 			return err
 		}
 
-		shouldWrite := true
 		if fileExists {
-			if skipExisting {
-				console.Infof("Skipped existing %s", filename)
-				continue
-			}
-			if overwriteExisting {
-				console.Infof("Overwriting existing %s", filename)
-			} else {
-				// Check if we have a global choice from previous prompts
-				if globalSkip {
-					console.Infof("Skipped existing %s", filename)
-					continue
-				}
-				// Prompt for this specific file
-				choice, err := console.Interactive{
-					Prompt:  fmt.Sprintf("File %s already exists. What would you like to do?", filename),
-					Options: []string{"skip", "skip-all", "quit"},
-					Default: "skip",
-				}.Read()
-				if err != nil {
-					return err
-				}
-
-				switch choice {
-				case "skip":
-					console.Infof("Skipped existing %s", filename)
-					continue
-				case "skip-all":
-					globalSkip = true
-					console.Infof("Skipped existing %s (and will skip all remaining)", filename)
-					continue
-				case "quit":
-					console.Infof("Exiting without making changes")
-					return nil
-				}
-			}
+			console.Infof("Skipped existing %s", filename)
+			continue
 		}
 
-		if shouldWrite {
-			dirPath := path.Dir(filePath)
-			err = os.MkdirAll(dirPath, os.ModePerm)
-			if err != nil {
-				return fmt.Errorf("Error creating directory %s: %w", dirPath, err)
-			}
-
-			err = os.WriteFile(filePath, content, 0o644)
-			if err != nil {
-				return fmt.Errorf("Error writing %s: %w", filePath, err)
-			}
-			console.Infof("✅ Created %s", filePath)
+		dirPath := path.Dir(filePath)
+		err = os.MkdirAll(dirPath, os.ModePerm)
+		if err != nil {
+			return fmt.Errorf("Error creating directory %s: %w", dirPath, err)
 		}
+
+		err = os.WriteFile(filePath, content, 0o644)
+		if err != nil {
+			return fmt.Errorf("Error writing %s: %w", filePath, err)
+		}
+		console.Infof("✅ Created %s", filePath)
 	}
 
 	console.Infof("\nDone! For next steps, check out the docs at https://cog.run/getting-started")
