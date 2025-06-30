4186d1fd906ea2f726e0427954b269219aa1ee10
Mark Phelps <209477+markphelps@users.noreply.github.com>
2025-06-30 14:03:06 -0400

    chore: fix test
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- pkg/cli/init_test.go
+++ pkg/cli/init_test.go
@@ -13,10 +13,6 @@ func TestInit(t *testing.T) {
 
 	require.NoError(t, os.Chdir(dir))
 
-	// Reset flags
-	skipExisting = false
-	overwriteExisting = false
-
 	err := initCommand(nil, []string{})
 	require.NoError(t, err)
 
@@ -29,10 +25,6 @@ func TestInitSkipExisting(t *testing.T) {
 	dir := t.TempDir()
 
 	require.NoError(t, os.Chdir(dir))
-
-	// Reset flags
-	skipExisting = false
-	overwriteExisting = false
 
 	// First run to create files
 	err := initCommand(nil, []string{})
@@ -47,9 +39,7 @@ func TestInitSkipExisting(t *testing.T) {
 	require.NoError(t, os.WriteFile(path.Join(dir, "predict.py"), []byte("test456"), 0o644))
 	require.NoError(t, os.WriteFile(path.Join(dir, ".dockerignore"), []byte("test789"), 0o644))
 
-	// Second run with skip-existing flag set
-	// mimics the behavior of `cog init --skip-existing`
-	skipExisting = true
+	// Second run should skip the files that already exist
 	err = initCommand(nil, []string{})
 	require.NoError(t, err)
 
@@ -69,50 +59,4 @@ func TestInitSkipExisting(t *testing.T) {
 	content, err = os.ReadFile(path.Join(dir, ".dockerignore"))
 	require.NoError(t, err)
 	require.Equal(t, []byte("test789"), content)
-}
-
-func TestInitOverwriteExisting(t *testing.T) {
-	dir := t.TempDir()
-
-	require.NoError(t, os.Chdir(dir))
-
-	// Reset flags
-	skipExisting = false
-	overwriteExisting = false
-
-	// First run to create files
-	err := initCommand(nil, []string{})
-	require.NoError(t, err)
-
-	require.FileExists(t, path.Join(dir, ".dockerignore"))
-	require.FileExists(t, path.Join(dir, "cog.yaml"))
-	require.FileExists(t, path.Join(dir, "predict.py"))
-
-	// update the file to show that its the same file after the second run
-	require.NoError(t, os.WriteFile(path.Join(dir, "cog.yaml"), []byte("test123"), 0o644))
-	require.NoError(t, os.WriteFile(path.Join(dir, "predict.py"), []byte("test456"), 0o644))
-	require.NoError(t, os.WriteFile(path.Join(dir, ".dockerignore"), []byte("test789"), 0o644))
-
-	// Second run with overwrite-existing flag set
-	// mimics the behavior of `cog init --overwrite-existing`
-	overwriteExisting = true
-	err = initCommand(nil, []string{})
-	require.NoError(t, err)
-
-	require.FileExists(t, path.Join(dir, ".dockerignore"))
-	require.FileExists(t, path.Join(dir, "cog.yaml"))
-	require.FileExists(t, path.Join(dir, "predict.py"))
-
-	// check that the files are NOT the same as the first run
-	content, err := os.ReadFile(path.Join(dir, "cog.yaml"))
-	require.NoError(t, err)
-	require.NotEqual(t, []byte("test123"), content)
-
-	content, err = os.ReadFile(path.Join(dir, "predict.py"))
-	require.NoError(t, err)
-	require.NotEqual(t, []byte("test456"), content)
-
-	content, err = os.ReadFile(path.Join(dir, ".dockerignore"))
-	require.NoError(t, err)
-	require.NotEqual(t, []byte("test789"), content)
 }
