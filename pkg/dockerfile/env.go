93be60db5c3af288467eb96373ceb168e3d4bb2d
Neville Li <neville.lyh@gmail.com>
2025-07-02 14:00:13 -0400

    Move to CDN for monobase matrix.json

--- pkg/dockerfile/env.go
+++ pkg/dockerfile/env.go
@@ -9,8 +9,6 @@ import (
 )
 
 const CogletVersionEnvVarName = "R8_COGLET_VERSION"
-const MonobaseMatrixHostVarName = "R8_MONOBASE_MATRIX_HOST"
-const MonobaseMatrixSchemeVarName = "R8_MONOBASE_MATRIX_SCHEME"
 
 func envLineFromConfig(c *config.Config) (string, error) {
 	vars := c.ParsedEnvironment()
@@ -33,20 +31,4 @@ func CogletVersionFromEnvironment() string {
 		host = "coglet"
 	}
 	return host
-}
-
-func MonobaseMatrixHostFromEnvironment() string {
-	host := os.Getenv(MonobaseMatrixHostVarName)
-	if host == "" {
-		host = "raw.githubusercontent.com"
-	}
-	return host
-}
-
-func MonobaseMatrixSchemeFromEnvironment() string {
-	scheme := os.Getenv(MonobaseMatrixSchemeVarName)
-	if scheme == "" {
-		scheme = "https"
-	}
-	return scheme
 }
