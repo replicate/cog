e9f0d96875183922d5b28435163233fe7cad0653
Will Sackfield <sackfield@replicate.com>
2025-07-30 14:35:29 -0400

    Allow wheel >= 1 to be installed

--- pkg/dockerfile/standard_generator.go
+++ pkg/dockerfile/standard_generator.go
@@ -424,7 +424,7 @@ RUN --mount=type=cache,target=/root/.cache/pip curl -s -S -L https://raw.githubu
 	export PYTHON_CFLAGS='-O3' && \
 	pyenv install-latest "%s" && \
 	pyenv global $(pyenv install-latest --print "%s") && \
-	pip install --upgrade pip setuptools "wheel<1"`, py, py) + `
+	pip install --upgrade pip setuptools wheel`, py, py) + `
 RUN rm -rf /usr/bin/python3 && ln -s ` + "`realpath \\`pyenv which python\\`` /usr/bin/python3 && chmod +x /usr/bin/python3", nil
 	// for sitePackagesLocation, kind of need to determine which specific version latest is (3.8 -> 3.8.17 or 3.8.18)
 	// install-latest essentially does pyenv install --list | grep $py | tail -1
