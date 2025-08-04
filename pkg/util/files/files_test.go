23b93cc6f6db5cd64d39991b49b312be2dfc30e0
Will Sackfield <sackfield@replicate.com>
2025-08-04 12:48:19 -0400

    Add integration test for 3.13 base images (#2485)
    * Add integration test for 3.13 base images
    
    * Add fallback to binary base64 URL decode
    
    * Python 3.13 adds some mime types that dataurl
    cannot recognise.
    * To support this, we detect if an error occurs
    during decoding and attempt to fix the URL so it
    is a generic binary decode.

--- pkg/util/files/files_test.go
+++ pkg/util/files/files_test.go
@@ -25,3 +25,10 @@ func TestWriteBadlyFormattedBase64DataURI(t *testing.T) {
 	_, err := WriteDataURLToFile("data:None;base64,SGVsbG8gVGhlcmU=", path)
 	require.NoError(t, err)
 }
+
+func TestWriteNotRecognisedBase64DataURL(t *testing.T) {
+	dir := t.TempDir()
+	path := filepath.Join(dir, "test-file")
+	_, err := WriteDataURLToFile("data:None;model/gltf-binary,SGVsbG8gVGhlcmU=", path)
+	require.NoError(t, err)
+}
