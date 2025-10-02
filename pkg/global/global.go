171b1d525118a586f85361163215958d4a2ec5a6
Mark Phelps <209477+markphelps@users.noreply.github.com>
2025-10-01 20:14:14 -0400

    feat: add registry migration support with credential fallback (#2541)

--- pkg/global/global.go
+++ pkg/global/global.go
@@ -1,13 +1,27 @@
 package global
 
+import "os"
+
+const (
+	DefaultReplicateRegistryHost = "r8.im"
+)
+
 var (
 	Version                 = "dev"
 	Commit                  = ""
 	BuildTime               = "none"
 	Debug                   = false
 	ProfilingEnabled        = false
-	ReplicateRegistryHost   = "r8.im"
+	ReplicateRegistryHost   = getDefaultRegistryHost()
 	ReplicateWebsiteHost    = "replicate.com"
 	LabelNamespace          = "run.cog."
 	CogBuildArtifactsFolder = ".cog"
 )
+
+func getDefaultRegistryHost() string {
+	// Priority: flag will override at runtime, but env var provides default
+	if host := os.Getenv("COG_REGISTRY_HOST"); host != "" {
+		return host
+	}
+	return DefaultReplicateRegistryHost
+}
