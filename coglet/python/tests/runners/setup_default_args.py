2bcc0a028c181efbde0c10c4db1a02a426eda80f
Mark Phelps <mphelps@cloudflare.com>
2026-01-23 16:01:31 -0500

    chore: rip coglet
    Signed-off-by: Mark Phelps <mphelps@cloudflare.com>

--- coglet/python/tests/runners/setup_default_args.py
+++
@@ -1,9 +0,0 @@
-from cog import BasePredictor
-
-
-class Predictor(BasePredictor):
-    def setup(self, example_arg: bool = False) -> None:
-        print(f'Example Arg: {example_arg}')
-
-    def predict(self) -> str:
-        return 'hello'
