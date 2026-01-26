4c48a94062bf070766d91a4c6115720783e96c1f
Michael Dwan <mdwan@cloudflare.com>
2026-01-26 09:17:55 -0700

    Add pkg/model Factory interface and DockerfileFactory
    Factory interface enables pluggable build backends.
    DockerfileFactory wraps existing image.Build() function.

---
+++ pkg/model/factory_test.go
@@ -0,0 +1,36 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/replicate/cog/pkg/docker/dockertest"
+	"github.com/replicate/cog/pkg/registry/registrytest"
+)
+
+func TestDockerfileFactory_Name(t *testing.T) {
+	docker := dockertest.NewMockCommand()
+	registry := registrytest.NewMockRegistryClient()
+
+	factory := NewDockerfileFactory(docker, registry)
+
+	require.Equal(t, "dockerfile", factory.Name())
+}
+
+func TestDockerfileFactory_ImplementsInterface(t *testing.T) {
+	docker := dockertest.NewMockCommand()
+	registry := registrytest.NewMockRegistryClient()
+
+	// Verify that DockerfileFactory implements the Factory interface
+	var _ Factory = NewDockerfileFactory(docker, registry)
+}
+
+func TestDefaultFactory_ReturnsDockerfileFactory(t *testing.T) {
+	docker := dockertest.NewMockCommand()
+	registry := registrytest.NewMockRegistryClient()
+
+	factory := DefaultFactory(docker, registry)
+
+	require.Equal(t, "dockerfile", factory.Name())
+}
