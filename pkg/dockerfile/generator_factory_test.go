3b914b0513d6a026e735ecfa3f3f66965b8c41ff
morgan fainberg <mfainberg@cloudflare.com>
2026-01-30 15:34:05 -0800

    warn on fast builds and fall back to standard

--- pkg/dockerfile/generator_factory_test.go
+++ pkg/dockerfile/generator_factory_test.go
@@ -1,6 +1,8 @@
 package dockerfile
 
 import (
+	"io"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -8,6 +10,7 @@ import (
 	"github.com/replicate/cog/pkg/config"
 	"github.com/replicate/cog/pkg/docker/dockertest"
 	"github.com/replicate/cog/pkg/registry/registrytest"
+	"github.com/replicate/cog/pkg/util/console"
 )
 
 func TestGeneratorFactory(t *testing.T) {
@@ -15,14 +18,41 @@ func TestGeneratorFactory(t *testing.T) {
 	build := config.Build{
 		PythonPackages: []string{"torch==2.5.1"},
 	}
-	config := config.Config{
+	cfg := config.Config{
 		Build: &build,
 	}
 	command := dockertest.NewMockCommand()
 	client := registrytest.NewMockRegistryClient()
-	generator, err := NewGenerator(&config, dir, true, command, true, client, true)
+
+	previousColor := console.ConsoleInstance.Color
+	previousLevel := console.ConsoleInstance.Level
+	console.SetColor(false)
+	console.SetLevel(console.InfoLevel)
+	defer func() {
+		console.SetColor(previousColor)
+		console.SetLevel(previousLevel)
+	}()
+
+	previousStderr := os.Stderr
+	stderrReader, stderrWriter, err := os.Pipe()
 	require.NoError(t, err)
-	require.Equal(t, generator.Name(), FAST_GENERATOR_NAME)
+	os.Stderr = stderrWriter
+	defer func() {
+		os.Stderr = previousStderr
+		_ = stderrReader.Close()
+		_ = stderrWriter.Close()
+	}()
+
+	generator, err := NewGenerator(&cfg, dir, true, command, true, client, true)
+	require.NoError(t, err)
+	require.Equal(t, STANDARD_GENERATOR_NAME, generator.Name())
+	require.NotNil(t, cfg.Build.CogRuntime)
+	require.True(t, *cfg.Build.CogRuntime)
+
+	require.NoError(t, stderrWriter.Close())
+	stderrOutput, err := io.ReadAll(stderrReader)
+	require.NoError(t, err)
+	require.Contains(t, string(stderrOutput), "experimental fast features, --x-fast / fast: true, are no longer supported. Use of flags/directives will error in a future release; falling back to standard build and generator.")
 }
 
 func TestGeneratorFactoryStandardGenerator(t *testing.T) {
