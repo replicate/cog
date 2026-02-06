bd9bc72afbed1f009728b900ec1deb24ac661495
morgan fainberg <code@tempusfrang.it>
2026-02-06 12:59:48 -0800

    implement run and serve commands
    run: builds base image, mounts project as /src, runs user command
    with GPU retry logic and port publishing.
    
    serve: same base image build, runs cog.server.http on container
    port 5000, maps to --port (default 8393).

--- pkg/cli/kong/run.go
+++ pkg/cli/kong/run.go
@@ -1,5 +1,16 @@
 package kong
 
+import (
+	"strconv"
+	"strings"
+
+	"github.com/replicate/cog/pkg/docker"
+	"github.com/replicate/cog/pkg/docker/command"
+	"github.com/replicate/cog/pkg/model"
+	"github.com/replicate/cog/pkg/registry"
+	"github.com/replicate/cog/pkg/util/console"
+)
+
 // RunCmd implements `cog run <command> [arg...]`.
 type RunCmd struct {
 	Command []string `arg:"" required:"" help:"Command and arguments to run" passthrough:""`
@@ -12,5 +23,53 @@ type RunCmd struct {
 }
 
 func (c *RunCmd) Run(g *Globals) error {
-	return nil // TODO
+	ctx := contextFromGlobals(g)
+
+	dockerClient, err := docker.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	src, err := model.NewSource(c.ConfigFile)
+	if err != nil {
+		return err
+	}
+
+	resolver := model.NewResolver(dockerClient, registry.NewRegistryClient())
+	m, err := resolver.BuildBase(ctx, src, c.BuildFlags.BuildBaseOptions())
+	if err != nil {
+		return err
+	}
+
+	gpus := c.GPUFlags.ResolveGPUs(m.HasGPU())
+	env := propagateRustLog(c.Env)
+
+	runOptions := command.RunOptions{
+		Args:    c.Command,
+		Env:     env,
+		GPUs:    gpus,
+		Image:   m.ImageRef(),
+		Volumes: []command.Volume{{Source: src.ProjectDir, Destination: "/src"}},
+		Workdir: "/src",
+	}
+
+	for _, portString := range c.Publish {
+		port, err := strconv.Atoi(portString)
+		if err != nil {
+			return err
+		}
+		runOptions.Ports = append(runOptions.Ports, command.Port{HostPort: port, ContainerPort: port})
+	}
+
+	console.Info("")
+	console.Infof("Running '%s' in Docker with the current directory mounted as a volume...", strings.Join(c.Command, " "))
+
+	err = docker.Run(ctx, dockerClient, runOptions)
+	if runOptions.GPUs == "all" && c.GPUFlags.IsAutoDetected() && err == docker.ErrMissingDeviceDriver {
+		console.Info("Missing device driver, re-trying without GPU")
+		runOptions.GPUs = ""
+		err = docker.Run(ctx, dockerClient, runOptions)
+	}
+
+	return err
 }
--- pkg/cli/kong/serve.go
+++ pkg/cli/kong/serve.go
@@ -1,5 +1,15 @@
 package kong
 
+import (
+	"strings"
+
+	"github.com/replicate/cog/pkg/docker"
+	"github.com/replicate/cog/pkg/docker/command"
+	"github.com/replicate/cog/pkg/model"
+	"github.com/replicate/cog/pkg/registry"
+	"github.com/replicate/cog/pkg/util/console"
+)
+
 // ServeCmd implements `cog serve`.
 type ServeCmd struct {
 	Port int      `help:"Port on which to listen" short:"p" default:"8393"`
@@ -10,5 +20,57 @@ type ServeCmd struct {
 }
 
 func (c *ServeCmd) Run(g *Globals) error {
-	return nil // TODO
+	ctx := contextFromGlobals(g)
+
+	dockerClient, err := docker.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	src, err := model.NewSource(c.ConfigFile)
+	if err != nil {
+		return err
+	}
+
+	resolver := model.NewResolver(dockerClient, registry.NewRegistryClient())
+	m, err := resolver.BuildBase(ctx, src, c.BuildFlags.BuildBaseOptions())
+	if err != nil {
+		return err
+	}
+
+	gpus := c.GPUFlags.ResolveGPUs(m.HasGPU())
+
+	args := []string{
+		"python",
+		"--check-hash-based-pycs", "never",
+		"-m", "cog.server.http",
+		"--await-explicit-shutdown", "true",
+	}
+
+	env := propagateRustLog(c.Env)
+
+	runOptions := command.RunOptions{
+		Args:    args,
+		Env:     env,
+		GPUs:    gpus,
+		Image:   m.ImageRef(),
+		Volumes: []command.Volume{{Source: src.ProjectDir, Destination: "/src"}},
+		Workdir: "/src",
+		Ports:   []command.Port{{HostPort: c.Port, ContainerPort: 5000}},
+	}
+
+	console.Info("")
+	console.Infof("Running '%s' in Docker with the current directory mounted as a volume...", strings.Join(args, " "))
+	console.Info("")
+	console.Infof("Serving at http://127.0.0.1:%d", c.Port)
+	console.Info("")
+
+	err = docker.Run(ctx, dockerClient, runOptions)
+	if runOptions.GPUs == "all" && c.GPUFlags.IsAutoDetected() && err == docker.ErrMissingDeviceDriver {
+		console.Info("Missing device driver, re-trying without GPU")
+		runOptions.GPUs = ""
+		err = docker.Run(ctx, dockerClient, runOptions)
+	}
+
+	return err
 }
