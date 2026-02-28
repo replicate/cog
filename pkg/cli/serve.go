fba87f3543623b41c21f17d36801fd023dc8f90e
Ben Firshman <bfirshman@cloudflare.com>
2026-02-27 16:28:14 -0800

    fix: skip adding labels to images for local-use commands (run/predict/serve/train)
    When running cog run, cog predict, cog serve, or cog train, the build
    was unnecessarily adding Cog metadata labels to the image. This added
    overhead (pip freeze, git info, base image layer inspection, extra
    Docker build step) for images that are only used locally and never
    distributed.
    
    This was a regression introduced in 1525f7f8 which replaced
    resolver.BuildBase() (no labels) with resolver.Build() (adds labels)
    for the serve/run/predict/train code paths.
    
    Adds a SkipLabels option to BuildOptions that short-circuits the
    label-adding phase in image.Build() when set.

--- pkg/cli/serve.go
+++ pkg/cli/serve.go
@@ -64,6 +64,7 @@ func serveBuildOptions(cmd *cobra.Command) model.BuildOptions {
 		UseCogBaseImage:  DetermineUseCogBaseImage(cmd),
 		ProgressOutput:   buildProgressOutput,
 		ExcludeSource:    true,
+		SkipLabels:       true,
 	}
 }
 
